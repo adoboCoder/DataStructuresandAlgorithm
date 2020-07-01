e651a211263e6b86f92a9d492ad3487addfc2932
Aim Verapornpongkul <ibaim@aims-mbp.lan>
2020-06-30 23:20:00 -0700

    First commit

---
+++ LCH_273IntegerToEnglishWords.java
@@ -0,0 +1,33 @@
+public class Solution {
+        private String[] lessThanTwenty = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", 
+                                "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+        private String[] tens = {"", "Ten", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+        private String [] thousands = {"", "Thousand", "Million", "Billion"};
+    public String numberToWords (int number){
+        if (number == 0){
+            return "Zero";
+        }
+        int i = 0;
+        String words = "";
+
+        while(number > 0){
+            if(number % 1000 != 0){
+                words = helper (number % 1000) + thousands[i] + " " + words;
+            }
+            number = number / 1000;
+
+        }
+        return words. trim();
+    }
+    
+    private String helper (int number){
+        if (number == 0) return "";
+        else if (number < 20 )
+            return lessThanTwenty[number] + " ";
+        else if(number < 100)
+            return tens[number / 10] + " " + helper(number);
+        else 
+            return lessThanTwenty[number / 100] + " Hundred " + helper(number % 100);
+        
+    }
+}
