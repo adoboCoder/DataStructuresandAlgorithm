e9e42972e7d3c0ccdc83a7429912c5da7be23e3e
Aim Verapornpongkul <averapornpongkul@co.marion.or.us>
2020-07-13 14:24:10 -0700

    First Commit

---
+++ LCM_199BinaryTreeRightSideView.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+
+        helper(result, root, 0);
+        return result;
+    }
+    
+    public void helper(List<Integer> result, TreeNode root, int depth){
+        if(root == null){
+            return;
+        }
+        if( depth == result.size())
+        result.add(root.val);
+        
+         helper(result, root.right, depth +1);
+         helper(result, root.left, depth +1);
+    }
+}
