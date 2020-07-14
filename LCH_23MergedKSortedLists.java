49ae871f96b9644b5c07dbb5c208cae892877f78
Aim Verapornpongkul <averapornpongkul@co.marion.or.us>
2020-07-14 15:26:54 -0700

    Fixed Syntax

--- LCH_23MergedKSortedLists.java
+++ LCH_23MergedKSortedLists.java
@@ -1,26 +1,20 @@
 // Facebook #1 asked question according to Leetcode as of 6/25/2020
 class Solution{
-    public ListNode mergeKList(ListNode[] lists){
-        if (lists == null || lists.length() == 0){
-            return null;
-        }
-// using Java 8 allows me to use lambda functions ((a,b) -> (a.val - b.val)) inline sorting!
-        PriorityQue <ListNode> queue = new PriorityQue<ListNode> (lists.length, (a,b) -> (a.val - b.val);
-
+    public ListNode mergeKLists(ListNode[] lists){
+        PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>(lists.length, (a,b)-> a.val-b.val);
         ListNode dummy = new ListNode(0);
         ListNode tail = dummy;
 
-        for(ListNode node: lists){
-            if (node != null){
-                que.add(node);
-            }
-        while (!que.isEmpty()){
-            tail.next = que.poll();
+        for(ListNode node: lists)
+            if (node != null)
+                queue.add(node);
+        while (!queue.isEmpty()){
+            tail.next = queue.poll();
             tail = tail.next;
-        }
-            if(tail.next != null){
-                que.add(tail.netx);
-            }
+        
+            if(tail.next != null)
+            queue.add(tail.next);
+            
         }
         return dummy.next;
     }
