6cb9404ebf971853608d95f7602f9b96e998a9d7
Aim Verapornpongkul <ibaim@aims-mbp.lan>
2020-07-22 14:19:49 -0700

    Spelling and misplaced curly brackets

--- LCH_23MergedKSortedLists.java
+++ LCH_23MergedKSortedLists.java
@@ -1,6 +1,5 @@
 class Solution{
     public ListNode mergeKLists(ListNode[] lists){
-<<<<<<< HEAD
         if (lists == null || lists.length == 0)return null;
         
         PriorityQueue <ListNode> queue = new PriorityQueue<ListNode> (lists.length, (a,b) -> (a.val - b.val));
@@ -9,13 +8,6 @@ class Solution{
         ListNode tail = dummy;
 
         for(ListNode node : lists)
-=======
-        PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>(lists.length, (a,b)-> a.val-b.val);
-        ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
-
-        for(ListNode node: lists)
->>>>>>> cae38e55d6a5dc58da86e48198d8f0bd828be043
             if (node != null)
                 queue.add(node);
         while (!queue.isEmpty()){
@@ -23,11 +15,7 @@ class Solution{
             tail = tail.next;
         
             if(tail.next != null)
-<<<<<<< HEAD
                 queue.add(tail.next);
-=======
-            queue.add(tail.next);
->>>>>>> cae38e55d6a5dc58da86e48198d8f0bd828be043
             
         }
         return dummy.next;
