0e82b5be7c58fa5acbb45bf9fc2fb7c7ea40fe23
Aim Verapornpongkul <AVerapornpongkul@co.marion.or.us>
2020-07-23 09:59:48 -0700

    Added Recursive Solution

--- LCE_21MergeTwoSortedLists.java
+++ LCE_21MergeTwoSortedLists.java
@@ -17,3 +17,31 @@ class Solution{
         return answer.next;
     }
 }
+
+// Time: O(n)
+// Space O(n)
+
+class Solution{
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        //Recurive Solution
+        if(l1 == null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+
+        if(l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else{
+            l2.next = mergeTwoLists(l1, l2.next)
+            return l2;
+        }
+    }
+
+}
+
+// Time: O(n)
+// Space O(n)
+
