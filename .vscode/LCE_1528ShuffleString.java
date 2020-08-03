e0f8de5b5bcca15f8746e96f6e9ab7d9d27d0f19
Aim Verapornpongkul <ibaim@Aims-MacBook-Pro.local>
2020-08-03 13:59:50 -0700

    First Commit

---
+++ .vscode/LCE_1528ShuffleString.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] c = new char[indices.length];
+        for(int i = 0; i < indices.length; i++){
+            c[indices[i]] = s.charAt(i);
+        }
+        return new String(c);
+    }
+}
+
+//Time: O(n)
+//Space: O(n)
