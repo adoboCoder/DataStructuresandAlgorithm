e7c9231f4299edbcc3cfe9f35f9141a628c00a11
Aim Verapornpongkul <AVerapornpongkul@co.marion.or.us>
2020-08-05 00:22:33 -0700

    First Commit

---
+++ LCM_56MergeIntervals.java
@@ -0,0 +1,25 @@
+class Solution {
+	public int[][] merge(int[][] intervals) {
+		if (intervals.length <= 1)
+			return intervals;
+		// Sort by ascending starting point
+		Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+		List<int[]> result = new ArrayList<>();
+        //NewInterval is the first item in intervals[]
+		int[] newInterval = intervals[0];
+        // Add first item to results set to start comparing.
+        result.add(newInterval);
+		for (int[] interval : intervals) {
+			if (interval[0] <= newInterval[1]){
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
+            } // Overlapping intervals, move the end if needed
+                
+			else {                             // Disjoint intervals, add the new interval to the list
+				newInterval = interval;
+				result.add(newInterval);
+			}
+		}
+		return result.toArray(new int[result.size()][]);
+	}
+}
