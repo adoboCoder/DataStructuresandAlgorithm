153523b2840416efc9a8c922dd80f996b9057ccf
Aim Verapornpongkul <AVerapornpongkul@co.marion.or.us>
2020-08-08 11:40:01 -0700

    Changed function  name to sinkIsland

--- LCM_200NumberOfIslands.java
+++ LCM_200NumberOfIslands.java
@@ -12,21 +12,21 @@ public class Solution {
         for (int i = 0; i < n; i++){
             for (int j = 0; j < m; j++)
                 if (grid[i][j] == '1') {
-                    DFSMarking(grid, i, j);
+                    sinkIsland(grid, i, j);
                     count++;
                 }
         }    
         return count;
     }
 
-    private void DFSMarking(char[][] grid, int i, int j) {
+    private void sinkIsland(char[][] grid, int i, int j) {
         if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1'){
           return;  
         } 
         grid[i][j] = '0';
-        DFSMarking(grid, i + 1, j);
-        DFSMarking(grid, i - 1, j);
-        DFSMarking(grid, i, j + 1);
-        DFSMarking(grid, i, j - 1);
+        sinkIsland(grid, i + 1, j);
+        sinkIsland(grid, i - 1, j);
+        sinkIsland(grid, i, j + 1);
+        sinkIsland(grid, i, j - 1);
     }
 }
