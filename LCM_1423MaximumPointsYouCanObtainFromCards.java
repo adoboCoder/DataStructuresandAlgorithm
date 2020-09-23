c5f9bb86f666580d3b950bf6dab542267d94bff6
Aim Verapornpongkul <AVerapornpongkul@co.marion.or.us>
2020-09-23 13:56:40 -0700

    formatting

--- LCM_1423MaximumPointsYouCanObtainFromCards.java
+++ LCM_1423MaximumPointsYouCanObtainFromCards.java
@@ -1,10 +1,11 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
         int max = 0, sum = 0;
-        for(int i = 0; i<k;i++) sum += cardPoints[i];
-        
+        for(int i = 0; i < k; i++) {
+            sum = sum + cardPoints[i];
+        }
         max = sum;
-        for(int i = 1; i<=k; i++){
+        for(int i = 1; i <= k; i++){
             sum = sum + cardPoints[cardPoints.length -i] - cardPoints[k -i];
             max = Math.max(max, sum);
         }
