c0df4091d1f4c2e405d7455b54f0422184590722
Aim Verapornpongkul <AVerapornpongkul@co.marion.or.us>
2020-10-07 11:12:04 -0700

    Added comments on how to explain the problem

--- LCE_1twoSum.java
+++ LCE_1twoSum.java
@@ -16,3 +16,14 @@ class Solution {
 
 //Time: O(n)
 //Space: O(n)
+
+/*
+ This problem utilizes a hashmap data structure, it uses the logic of target - nums[index] to find the corresponding 
+operand in the hash table.  If there isn't a match in the hashTable, then put the values and the lookup index in the hash map 
+hash.put(nums[i],i)
+
+If there is a match, store the following in the result array of size 2; size is two because of it takes two operands to sum up to the target.
+result[0] would be index
+result[1] would be a hash map lookup hash.get(target - nums[i])
+
+*/
