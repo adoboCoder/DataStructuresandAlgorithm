b1ce0306547c7429b926ea86b1aa7bc4386d3b81
Aim Verapornpongkul <AVerapornpongkul@co.marion.or.us>
2020-10-08 14:18:08 -0700

    Added comments regarding the solution to the problem

--- LCE_53MaximumSubarray.java
+++ LCE_53MaximumSubarray.java
@@ -1,12 +1,40 @@
-class Solution{
-    public int maxSubArray(int[] A){
-        int max = A[0];
-        int maxEnd = A[0];
-
-        for(int i = 1; i < A.length; i++){
-            maxEnd = Math.max(maxEnd + A[i], A[i]);
-            max = Math.max(max, maxEnd);
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int tempMax = nums[0];
+        
+        for(int i = 1; i < nums.length; i++){
+            tempMax = Math.max(nums[i], tempMax + nums[i]);
+            max = Math.max(tempMax, max);
         }
         return max;
     }
 }
+
+/*
+nums = [-2,1,-3,4,-1,2,1,-5,4] 
+
+We need two variables to store a temporary max, and while computing a temp max we would continuously compare with previous 
+temp maxes to get a currentMax.
+
+For example:
+
+tempMax = -2 // The first element of the nums array (nums[0])
+
+by adding the next element nums[1] (1) 
+
+we would be choosing a new max between:
+
+1 and -2 + 1 (new contender)
+
+the new tempMax is now 1.
+
+
+The same prcoess can be done to find the max, initially tempMax is also the first element. 
+After the first iteration, it becomes -1 
+
+
+
+See Kadane's Algorithm for more details.
+
+*/
