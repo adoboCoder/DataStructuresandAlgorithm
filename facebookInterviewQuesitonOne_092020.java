68fb6c2eb9352db5cccfab1ca63c4251a83bfd69
Aim Verapornpongkul <ibaim@aims-mbp.lan>
2020-11-03 09:56:36 -0800

    Still working on this problem

--- facebookInterviewQuesitonOne_092020.java
+++ facebookInterviewQuesitonOne_092020.java
@@ -1,28 +1,26 @@
-
 import java.io.*; 
+import java.util.*;
 
-public class facebookInterviewQuesitonOne_092020{
-    public static double[] FindMovingAverage(int[] arr, int n){
-        double [] result = new double[n];
-        int start = 0;
-        int end = n - 1;
-        for(int i = 0; i < arr.length - n + 1; i++){
-            double currentSum = 0.0;
-          for(int j = 0; j < arr.length; j++){
-              currentSum = currentSum + arr[i + j];
-              result[i] = currentSum / n;
-          }
-        }
-        return result;
-    }
+// public class facebookInterviewQuesitonOne_092020{
+//     public static double[] FindMovingAverage(int[] arr, int n){
+//         double [] result = new double[n];
+//         for(int i = 0; i < arr.length - n; i++){
+//             double currentSum = 0.0;
+//           for(int j = 0; j < arr.length; j++){
+//               currentSum = currentSum + arr[i + j];
+//               result[i] = currentSum / n;
+//           }
+//         }
+//         return result;
+//     }
 
     public static void main(String[] args){
         int [] arr = {1, 2, 3, 4, 5, 6, 7, 8};
-        int n = 4;
-        double [] out = FindMovingAverage(arr, n);
-        for(double d : out){
-            System.out.println("[" + d +"]");
+        //int n = 4;
+        //double [] out = FindMovingAverage(arr, n);
+        for(int i = 0; i < arr.length; i++){
+            System.out.println("[" + arr[i] +"]");
         }
-        System.out.println(FindMovingAverage(arr, n));
+        
     }
-}
+//}
