0d8ae521590dd2594830039e74f15f7734cdf6f9
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2020-11-10 09:59:38 -0800

    fixed spacing

--- LCM_1138AlphabetBoardPath.java
+++ LCM_1138AlphabetBoardPath.java
@@ -8,35 +8,35 @@ class Google {
             StringBuilder sb = new StringBuilder();
             for(char c : target.toCharArray()){
     //target point (i,j)
-                int i = (c-'a') / rows;  
-                int j = (c-'a') % rows ;  
+                int i = (c - 'a') / rows;  
+                int j = (c - 'a') % rows ;  
     //now we compare (x,y) (i,j) ==> move x y to i, j
     //we need to handle special case "z"  cause z does not have the same length as other rows.
-                if(c=='z'){
+                if(c == 'z'){
     //move to L first to avoid indexOutOfBound
-                    while(y>j){
+                    while(y > j){
                         sb.append('L');
                         y--;
                     }
     //and we know once we move to most L (y=0) and we can only go down as "z" is at last row. 
-                    while(x<i){
+                    while(x < i){
                         sb.append('D');
                         x++;
                     }
                 }else{
-                    while(x<i){
+                    while(x < i){
                         sb.append('D');
                         x++;
                     }
-                    while(x>i){
+                    while(x > i){
                         sb.append('U');
                         x--;
                     }
-                    while(y<j){
+                    while(y < j){
                         sb.append('R');
                         y++;
                     }
-                    while(y>j){
+                    while(y > j){
                         sb.append('L');
                         y--;
                     }
