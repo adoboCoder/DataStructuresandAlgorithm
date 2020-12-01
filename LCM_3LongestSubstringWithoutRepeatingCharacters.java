41984685cbaa0eb48baec14124eda5bec09378ef
Aim Verapornpongkul <AVerapornpongkul@co.marion.or.us>
2020-12-01 09:05:05 -0800

    First Commit

---
+++ LCM_3LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,27 @@
+class Solution {
+     public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0){
+          return 0;  
+        } 
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max = 0;
+        for (int i=0, j=0; i < s.length(); i++){
+            if (map.containsKey(s.charAt(i))){
+                j = Math.max(j, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - j + 1);
+        }
+        return max;
+    }
+}
+
+/* 
+Two pointer technique while  keeping a hashmap with of the characters that appear in the string.
+
+
+Time: O(n)
+Space: O(n)
+
+
+*/
