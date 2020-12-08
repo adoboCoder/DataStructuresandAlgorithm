bb0ec1b3f40c858145fc43009e7a1756f2cd37cc
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2020-12-08 10:39:06 -0800

    Spelling

--- LCM_207CourseSchedule.java
+++ LCM_207CourseSchedule.java
@@ -57,7 +57,7 @@ public class Solution{
     }
 }
 /*
-This is a cycle detection problem.  The problem is solved by running a dfs in a directed graph (arrows pointing from node-to-done).  
+This is a cycle detection problem.  The problem is solved by running a dfs in a directed graph (arrows pointing from node-to-node).  
 For example, n = 5 prerequisites[][] = [[0,1], [1,2], [3,2], [4,3], [2,4]]
 
 To take course 0, course 1 must be taken first.
