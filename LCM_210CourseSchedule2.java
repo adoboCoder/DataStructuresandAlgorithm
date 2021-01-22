1342352af180c1868cee2b4984bde7d286079f77
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-01-22 11:14:15 -0800

    First commit

---
+++ LCM_210CourseSchedule2.java
@@ -0,0 +1,46 @@
+public class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        //Set up adjacency list
+        List<List<Integer>> adj = new ArrayList<>(numCourses);
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(i, new ArrayList<>());
+        }
+        //Poplulate adjacency list
+        for (int i = 0; i < prerequisites.length; i++) {
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+        //Set up visited array
+        boolean[] visited = new boolean[numCourses];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (!topologicalSort(adj, i, stack, visited, new boolean[numCourses])){
+              return new int[0];  
+            } 
+        }
+        int i = 0;
+        int[] result = new int[numCourses];
+        while (!stack.isEmpty()) {
+            result[i] = stack.pop();
+            i++;
+        }
+        return result;
+    }
+    
+    private boolean topologicalSort(List<List<Integer>> adj, int v, Stack<Integer> stack, boolean[] visited, boolean[] isLoop) {
+        if (visited[v]){
+          return true;  
+        } 
+        if (isLoop[v]){
+          return false;  
+        } 
+        isLoop[v] = true;
+        for (Integer u : adj.get(v)) {
+            if (!topologicalSort(adj, u, stack, visited, isLoop)){
+              return false;  
+            } 
+        }
+        visited[v] = true;
+        stack.push(v);
+        return true;
+    }
+}
