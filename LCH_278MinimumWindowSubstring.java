cd90ef7c6c73bc9d7ef4d3ed2006dde92774fe50
Aim Verapornpongkul <AVerapornpongkul@co.marion.or.us>
2021-01-31 23:02:17 -0800

    First Commit

---
+++ LCH_278MinimumWindowSubstring.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        int[] map = new int[128]; //ASCII Characters 
+        
+        //Initialize map for lookup
+        for(char c : t.toCharArray()) {
+            map[c]++;
+        }
+        
+        //Set pointers
+        int start = 0;
+        int end = 0;
+        //Set Counter
+        int counter = t.length();
+        
+        int minLength = Integer.MAX_VALUE;
+        int minStart = 0;
+        
+        while (end < s.length()) {
+      final char c1 = s.charAt(end);
+      if (map[c1] > 0){
+        counter--;     
+      }
+      map[c1]--;
+      end++;
+      while (counter == 0) {
+        if (minLength > end - start) {
+          minLength = end - start;
+          minStart = start;
+        }
+        final char c2 = s.charAt(start);
+        map[c2]++;
+        if (map[c2] > 0) {
+          counter++;  
+        } 
+        start++;
+      }
+    }
+
+    return minLength == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLength);
+  }
+}
