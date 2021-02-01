8858171d7f4eb9dec2a338c122c0bd8effba43a5
Aim Verapornpongkul <AVerapornpongkul@co.marion.or.us>
2021-01-31 23:09:51 -0800

    Added Time and space

--- LCH_76MinimumWindowSubstring.java
+++ LCH_76MinimumWindowSubstring.java
@@ -17,26 +17,29 @@ class Solution {
         int minStart = 0;
         
         while (end < s.length()) {
-      final char c1 = s.charAt(end);
-      if (map[c1] > 0){
-        counter--;     
-      }
-      map[c1]--;
-      end++;
-      while (counter == 0) {
-        if (minLength > end - start) {
-          minLength = end - start;
-          minStart = start;
-        }
-        final char c2 = s.charAt(start);
-        map[c2]++;
-        if (map[c2] > 0) {
-          counter++;  
-        } 
-        start++;
-      }
+            final char c1 = s.charAt(end);
+            if (map[c1] > 0){
+                counter--;     
+            }
+                map[c1]--;
+                end++;
+                while (counter == 0) {
+                    if (minLength > end - start) {
+                    minLength = end - start;
+                    minStart = start;
+                    }
+                    final char c2 = s.charAt(start);
+                    map[c2]++;
+                    if (map[c2] > 0) {
+                        counter++;  
+                    } 
+                    start++;
+                }
     }
 
     return minLength == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLength);
   }
 }
+
+//  Time:  O(s+t)
+//  Space:  O(n)
