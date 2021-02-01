faa1bce64580093850c1382deb0b8e79b3081e23
Aim Verapornpongkul <AVerapornpongkul@co.marion.or.us>
2021-01-31 23:29:38 -0800

    First Commit

---
+++ LCE_283Move_Zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        int j = 0;
+        while(i < nums.length && j < nums.length){
+            if(nums[j] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;   
+                i++;
+            }    
+            j++;
+        }   
+    }
+}
+
+// Time: O(n)
+// Space:O(1)
