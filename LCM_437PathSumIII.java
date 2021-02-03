8bd94db0b101ccd18fbab454c7df98f62f9e34f2
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-02-03 09:57:39 -0800

    First commit

---
+++ LCM_437PathSumIII.java
@@ -0,0 +1,17 @@
+
+class Solution {
+   public int pathSum(TreeNode root, int sum) {
+    if (root == null){
+        return 0;  
+    }
+    return dfs(root, sum, 0) + pathSum(root.left, sum) + pathSum(root.right, sum);
+  }
+
+  private int dfs(TreeNode root, int sum, int current) {
+    if (root == null){
+        return 0;
+    }
+    current += root.val;
+    return (current == sum ? 1 : 0) + dfs(root.left, sum, current) + dfs(root.right, sum, current);
+  }
+}
