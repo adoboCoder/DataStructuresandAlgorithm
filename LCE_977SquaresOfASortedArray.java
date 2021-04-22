46d2d09919308f4fbab76073c424e7fb5ce6cfe3
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-04-22 10:19:31 -0700

    First Commit

---
+++ LCE_977SquaresOfASortedArray.java
@@ -0,0 +1,19 @@
+class Solution{
+    public int[] sortedSquears(int[] nums){
+        int left = 0
+        int right = nums.length - 1;
+        int[] result = new int[nums.length];
+        
+        for(int i = right; right >= 0; right--){
+            if(Math.abs(nums[left]) > Math.abs(nums[right])){
+                result[i] = nums[left] * nums[left];
+                left++;
+            }
+            else{
+                result[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return result;
+    }
+}
