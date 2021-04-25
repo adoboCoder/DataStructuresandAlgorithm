bf6b78867a7caedc99926069735ddacac720cd7c
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2021-04-24 23:13:48 -0700

    First Commit

---
+++ LCM_1448CountGoodNodesInBinaryTree.java
@@ -0,0 +1,26 @@
+class Solution{
+    int good;
+    public int goodNodes(TreeNode root){
+        good = 0;
+        int max = Integer.MIN_VALUE;
+        helper(root, max);
+        return good;
+    }
+    
+    public void helper(TreeNode root, int max){
+        if(root == null){
+            return;
+        }
+        
+        if(root.val >= max){
+            good++;
+        }
+        max = Math.max(max, root.val);
+        helper(root.left, max);
+        helper(root.right, max);
+    }
+}
+
+//Time O(n)
+//Space O(h)
+
