967a8da223b3cdaf178e754de5dc95dc8c06fbad
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-04-27 15:49:16 -0700

    Fixed sum and max to be initialized to cost[0]

--- LCM_1578MinimumDeletionCostToAvoidRepeatingLetters.java
+++ LCM_1578MinimumDeletionCostToAvoidRepeatingLetters.java
@@ -1,18 +1,18 @@
 class Solution{
-    public int minCost(String s, int[] cost){
+    public int minCost(String s, int[] cost) {
        char[] arr = s.toCharArray();
        int result = 0;
-       int sum = arr[0];
-       int max = arr[0];
+       int sum = cost[0];
+       int max = cost[0];
 
        for(int i = 1; i < arr.length; i++){
            if(arr[i] != arr[i-1]){
-               result = result + sum - max;
+               result += sum - max;
                sum = 0;
                max = 0;
            }
            max = Math.max(max, cost[i]);
-           sum = sum + cost[i];
+           sum += cost[i];
        }
        return result + sum - max;
     }
