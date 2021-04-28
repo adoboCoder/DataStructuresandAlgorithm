8e713b909e489b61db114667e93be7637e3f2b79
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-04-28 15:29:44 -0700

    first commit

--- LCM_1647MinimumDeletionsToMakeCharacterFrequenciesUnique.java
+++ LCM_1647MinimumDeletionsToMakeCharacterFrequenciesUnique.java
@@ -1,12 +1,12 @@
 class Solution{
     public int minDeletions(String s){
         int[] freqCount = new int[26];
+        int count = 0;
 
         for(char c : s.toCharArray()){
             freqCount[c - 'a']++;
         }
         Set<Integer> hs = new HashSet<>();
-        int count = 0;
         for(int i : freqCount){
             while(i != 0 && hs.contains(i)){
                 i--;
