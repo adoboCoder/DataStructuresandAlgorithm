c8dd5b1bc1ddd77a6038826591da0d1257713bdc
Aim Verapornpongkul <AimVerapornpongkul@gmail.com>
2021-05-06 13:31:15 -0700

    First commit

---
+++ Prime.java
@@ -0,0 +1,21 @@
+class Prime{
+    public static boolean isPrime(int n){
+        if (n <= 1)
+        return false;
+
+        for (int i = 2; i < n; i++){
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main (String[] args) {
+        int n = 100;
+
+        for(int i = 1; i <= n; i++){
+            String b = Prime.isPrime(i) == true ? "is a prime number." : "is not a prime number.";
+            System.out.println(i + " " + b); 
+        }
+    }
+}
