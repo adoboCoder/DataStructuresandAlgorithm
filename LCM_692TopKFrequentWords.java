2a87c12271ab6a032d40e3f4884972372627f85c
Aim Verapornpongkul <AimVerapornpongkul@gmail.com>
2021-05-17 22:59:01 -0700

    First commit

---
+++ LCM_692TopKFrequentWords.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class LCM_692TopKFrequentWords {
+    public static List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> map = new HashMap<>();
+        List<String> result = new ArrayList<>();
+        for(String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
+                 (a,b) -> a.getValue()==b.getValue() ? b.getKey().compareTo(a.getKey()) : a.getValue() - b.getValue()
+        );
+
+        for(Map.Entry<String, Integer> entry: map.entrySet())
+        {
+            pq.offer(entry);
+            if(pq.size()>k)
+                pq.poll();
+        }
+
+        while(!pq.isEmpty())
+        result.add(0, pq.poll().getKey());
+    
+    return result;
+    }
+
+    public static void main(String[] args) {
+        List<String> result = LCM_692TopKFrequentWords.topKFrequent(new String[] { "i", "love", "leetcode", "i", "love", "coding" }, 2);
+        System.out.println("Here are the K frequent words: " + result);
+    }
+}
