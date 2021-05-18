9740a7ace0baabba4c02bc3ad8102af3d7b0e8ff
Aim Verapornpongkul <AimVerapornpongkul@gmail.com>
2021-05-17 22:58:45 -0700

    Change

--- Prime.java
+++ Prime.java
@@ -1,17 +1,23 @@
 class Prime{
     public static boolean isPrime(int n){
         if (n <= 1)
-        return true;
+            return false;
+        if (n <= 3)
+            return true;
+  
 
-        for (int i = 2; i < n; i++){
-            if (n % i == 0)
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+  
+        for (int i = 5; i * i <= n; i = i + 6)
+            if (n % i == 0 || n % (i + 2) == 0)
                 return false;
-        }
+  
         return true;
     }
 
     public static void main (String[] args) {
-        int n = 25;
+        int n = 2000000;
 
         for(int i = 1; i <= n; i++){
             String b = Prime.isPrime(i) == true ? "is a prime number." : "is not a prime number.";
