823d815526652fd78cfcb707d861d07b67bc61c5
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-05-24 14:35:42 -0700

    Modified driver parameters

--- LCM_560SubarraySumEqualsK.java
+++ LCM_560SubarraySumEqualsK.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 class LCM_560SubarraySumEqualsK{
     public static int subarraySum(int[] nums, int k){
-        if(nums.length == 0) return 0;
-
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int result = 0;
         int sum = 0;
-        map.put(0,1);
-        for(int i : nums){
-            sum += i;
-            if(map.containsKey(sum - k)){
+        int result = 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        for(int i = 0; i < nums. length; i++) {
+            sum = sum + nums[i];
+
+            if(map.containsKey(sum - k)) {
                 result = result + map.get(sum - k);
-            }    
+            }
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return result;
     }
 
     public static void main(String[] args) {
-        int[] test = new int[] {1, 2, 3, 4, 5};
+        int[] test = new int[] {1, 1, 1, 1, 2};
         System.out.println(Arrays.toString(test));
-        
         System.out.println(LCM_560SubarraySumEqualsK.subarraySum(test, 3));
     }
 }
