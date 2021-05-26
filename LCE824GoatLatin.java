51fae2df1926aef56a3dc5d82edb3fba00193413
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-05-26 14:46:27 -0700

    First commit

---
+++ LCE824GoatLatin.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class LCE824GoatLatin {
+    public static String toGoatLatin(String S){
+        Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e','i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+
+        if(S == null || S.length() == 0){
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        StringBuilder suffix = new StringBuilder("a");
+
+        for(String word : S.split(" ")) {
+            if(sb.length() != 0) {
+                sb.append(" ");
+            }
+
+            char c = word.charAt(0);
+
+            if(vowels.contains(c)) {
+                sb.append(word);
+            }
+            else {
+                sb.append(word.substring(1));
+                sb.append(c);
+            }
+
+            sb.append("ma").append(suffix);
+            suffix.append("a");
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] agrs) {
+        System.out.println(LCE824GoatLatin.toGoatLatin("I Speak Goat Latin"));
+        
+    }
+}
