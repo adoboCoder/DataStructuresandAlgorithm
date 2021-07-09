ad4d25c4eacba6915810054661df1fbe62050f2a
Kamonchanok Tankul <louktan@kamonchanoksair.lan>
2021-07-08 21:43:52 -0700

    Added driver method

--- LCM_695MaxAreaOfIsland.java
+++ LCM_695MaxAreaOfIsland.java
@@ -1,5 +1,5 @@
 class LCM_695MaxAreaOfIsland {
-    public int maxAreaOfIsland(int[][] grid) {
+    public static int maxAreaOfIsland(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         int maxArea = 0;
@@ -9,17 +9,30 @@ class LCM_695MaxAreaOfIsland {
                 if(grid[i][j] == 1){
                     maxArea = Math.max(maxArea,dfs(grid, i, j));  
                 }
-                
             }
         }
         return maxArea;
     }
     
-    public int dfs(int[][] grid, int i, int j) {
+    public static int dfs(int[][] grid, int i, int j) {
         if(i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == 1) {
             grid[i][j] = 0;
             return 1 + dfs(grid, i - 1, j) + dfs(grid, i + 1, j) + dfs(grid, i, j + 1) + dfs(grid, i, j - 1);
         }
         return 0;
+    }
+    public static void main(String[] args) {
+        //Declare 2d matrix
+        int[][] grid = {
+                        {0,0,1,0,0,0,0,1,0,0,0,0,0},
+                        {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                        {0,1,1,0,1,0,0,0,0,0,0,0,0},
+                        {0,1,0,0,1,1,0,0,1,0,1,0,0},
+                        {0,1,0,0,1,1,0,0,1,1,1,0,0},
+                        {0,0,0,0,0,0,0,0,0,0,1,0,0},
+                        {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                        {0,0,0,0,0,0,0,1,1,0,0,0,0}
+                        };
+        System.out.println(LCM_695MaxAreaOfIsland.maxAreaOfIsland(grid));
     }
 }
