6895973e7fb0277101b1df35b86907e9483fc3cd
ibaim <thisisaim@gmail.com>
2021-07-23 14:49:44 -0700

    Changed variable name to make the prompt more easily understood

--- LCM_1423MaximumPointsYouCanObtainFromCards.java
+++ LCM_1423MaximumPointsYouCanObtainFromCards.java
@@ -1,15 +1,16 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
-        int max = 0, sum = 0;
+        int max = 0;
+        int sum = 0;
         for(int i = 0; i < k; i++){
           sum += cardPoints[i];  
         } 
         
         max = sum;
         for(int i = 1; i <= k; i++){
-            int x =  cardPoints[cardPoints.length -i];
-            int y = cardPoints[k - i];
-            sum = sum + x - y;
+            int right =  cardPoints[cardPoints.length -i];
+            int left = cardPoints[k - i];
+            sum = sum + right - left;
             max = Math.max(max, sum);
         }
         return max;
