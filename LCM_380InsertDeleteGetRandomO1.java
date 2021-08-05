a5c4610dc44c6fa7868c6dc7965572522c1a8812
ibaim <thisisaim@gmail.com>
2021-08-04 23:58:03 -0700

    First commit

---
+++ LCM_380InsertDeleteGetRandomO1.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class RandomizedSet {
+    private List<Integer> list;
+    private HashMap<Integer, Integer> map;
+    java.util.Random random = new java.util.Random();
+    public RandomizedSet() {
+        list = new ArrayList<Integer>();
+        map = new HashMap<Integer, Integer>();
+    }
+
+    public boolean insert(int val) {
+        if(map.containsKey(val)) {
+            return false;
+        }
+        list.add(val);
+        map.put(val, list.size() - 1);
+        return true;
+    }
+    public boolean remove (int val) {
+        if(!map.containsKey(val)){
+            return false;
+        }
+        if(map.get(val) < list.size() - 1){
+            int index = map.get(val);
+            int last = list.get(list.size() - 1);
+            list.set(index, last);
+            map.put(last, index);
+        }
+        map.remove(val);
+        list.remove(list.size() - 1);
+        return true;
+
+    }
+
+    public int getRandom() {
+        return list.get(random.nextInt(list.size()));
+    }
+}
