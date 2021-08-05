8a93b56da2d81f422485a48ee685796d4fe41a47
ibaim <thisisaim@gmail.com>
2021-08-04 23:39:52 -0700

    made code more readable

--- LCM_523ContinuousSubArraySum.java
+++ LCM_523ContinuousSubArraySum.java
@@ -1,21 +1,25 @@
 class Solution {
-    public boolean checkSubarraySum(int[] nums, int k) {
-     Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        map.put(0,-1);
-     int runningSum = 0;
-     for (int i=0;i<nums.length;i++) {
-         runningSum += nums[i];
-         if (k != 0){
-             runningSum %= k; 
-         } 
-         Integer prev = map.get(runningSum);
-         if (prev != null) {
-             if (i - prev > 1) {
-                 return true;
-             }
-         }
-         else map.put(runningSum, i);
-     }
-     return false;
-     }
- }
+    public boolean checkSubarraySum (int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        
+        int runningSum = 0;
+        
+        for(int i = 0; i < nums.length; i ++) {
+            runningSum = runningSum + nums[i];
+            if(k != 0) {
+                runningSum = runningSum % k;
+            }
+            
+            Integer prev = map.get(runningSum);
+            
+            if(prev != null) {
+                if(i - prev > 1) {
+                    return true;
+                }
+            }
+            else map.put(runningSum, i);
+        }
+        return false;
+    }
+}
