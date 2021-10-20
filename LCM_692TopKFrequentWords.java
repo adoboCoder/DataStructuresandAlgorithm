541efb89fead4d0071e3e99016df0554c579427a
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-10-20 12:20:06 -0700

    Format

--- LCM_692TopKFrequentWords.java
+++ LCM_692TopKFrequentWords.java
@@ -5,11 +5,9 @@ class LCM_692TopKFrequentWords {
 
         List<String> result = new ArrayList<>();
         Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < words.length; i++) {
-            if (map.containsKey(words[i]))
-                map.put(words[i], map.get(words[i]) + 1);
-            else
-                map.put(words[i], 1);
+        
+        for(String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
         }
 
         PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>((a,
