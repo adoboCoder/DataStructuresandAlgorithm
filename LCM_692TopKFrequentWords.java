7f6374604eb14ac0fd7ca7c8bec64596858ef2be
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-10-20 07:47:56 -0700

    Update to implementation

--- LCM_692TopKFrequentWords.java
+++ LCM_692TopKFrequentWords.java
@@ -2,37 +2,34 @@ import java.util.*;
 
 class LCM_692TopKFrequentWords {
     public static List<String> topKFrequent(String[] words, int k) {
-        
-        List<String> result = new LinkedList<>();
+
+        List<String> result = new ArrayList<>();
         Map<String, Integer> map = new HashMap<>();
-        for(int i=0; i<words.length; i++)
-        {
-            if(map.containsKey(words[i]))
-                map.put(words[i], map.get(words[i])+1);
+        for (int i = 0; i < words.length; i++) {
+            if (map.containsKey(words[i]))
+                map.put(words[i], map.get(words[i]) + 1);
             else
                 map.put(words[i], 1);
         }
-        
-        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
-                 (a,b) -> a.getValue()==b.getValue() ? b.getKey().compareTo(a.getKey()) : a.getValue()-b.getValue()
-        );
-        
-        for(Map.Entry<String, Integer> entry: map.entrySet())
-        {
+
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>((a,
+                b) -> a.getValue() == b.getValue() ? b.getKey().compareTo(a.getKey()) : a.getValue() - b.getValue());
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
             pq.offer(entry);
-            if(pq.size()>k)
+            if (pq.size() > k)
                 pq.poll();
         }
 
-        while(!pq.isEmpty())
+        while (!pq.isEmpty())
             result.add(0, pq.poll().getKey());
-        
+
         return result;
     }
 
-
     public static void main(String[] args) {
-        List<String> result = LCM_692TopKFrequentWords.topKFrequent(new String[] { "i", "love", "leetcode", "i", "love", "coding" }, 2);
-        System.out.println("Here are the K frequent words:" +  result);
+        List<String> result = LCM_692TopKFrequentWords
+                .topKFrequent(new String[] { "i", "love", "leetcode", "i", "love", "coding" }, 2);
+        System.out.println("Here are the K frequent words:" + result);
     }
 }
