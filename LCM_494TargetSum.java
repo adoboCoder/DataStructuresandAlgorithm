3fbccf08bef855ca52114330380a8d1bce0f67ab
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-10-21 10:06:13 -0700

    changed approach

--- LCM_494TargetSum.java
+++ LCM_494TargetSum.java
@@ -1,21 +1,25 @@
+import java.util.*;
 
 class LCM_494TargetSum {
     public static int findTargetSumWays(int[] nums, int s) {
-        int sum = 0;
-        for (int n : nums)
-            sum += n;
-        return sum < s || (s + sum) % 2 > 0 ? 0 : subsetSum(nums, (s + sum) >>> 1); 
-    }   
-    public static int subsetSum(int[] nums, int s) {
-        int[] dp = new int[s + 1]; 
-        dp[0] = 1;
-        for (int n : nums)
-            for (int i = s; i >= n; i--)
-                dp[i] += dp[i - n]; 
-        return dp[s];
-    } 
+        Map<Integer, Integer> dp = new HashMap<>();
+        dp.put(0, 1);
+
+        for (int num : nums) {
+            Map<Integer, Integer> dp2 = new HashMap<>();
+            for (int tempSum : dp.keySet()) {
+                int key1 = tempSum + num;
+                dp2.put(key1, dp2.getOrDefault(key1, 0) + dp.get(tempSum));
+                int key2 = tempSum - num;
+                dp2.put(key2, dp2.getOrDefault(key2, 0) + dp.get(tempSum));
+            }
+            dp = dp2;
+        }
+        return dp.getOrDefault(s, 0);
+    }
+
     public static void main(String[] args) {
-        int[] nums = {1, 1, 1, 1, 1};
+        int[] nums = { 1, 1, 1, 1, 1 };
         int s = 3;
         System.out.println(LCM_494TargetSum.findTargetSumWays(nums, s));
     }
