67dd268619f628487bacd845c5d24d6e65790188
Kamonchanok Tankul <kamonchanoktankul@kamonchanoksair.lan>
2021-11-11 22:24:58 -0800

    First commit

---
+++ LCM735_AsteroidCollision.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class LCM735_AsteroidCollision {
+    public static int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+
+        for(int a : asteroids) {
+            if(a > 0) {
+                stack.push(a);
+            }
+            else {
+                while(!stack.isEmpty() && stack.peek() > 0 && stack.peek() < Math.abs(a)) {
+                    stack.pop();
+                }
+                if(stack.isEmpty() || stack.peek() < 0) {
+                    stack.push(a);
+                }
+                else if(a + stack.peek() == 0) {
+                    stack.pop();
+                }
+
+            }
+        }
+        int[] result = new int[stack.size()];
+
+        for(int i = result.length -1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] asteroids = {5, 10, -5};
+        System.out.println(Arrays.toString(LCM735_AsteroidCollision.asteroidCollision(asteroids)));
+
+        int[] asteroids2 = {5, -5};
+        System.out.println(Arrays.toString(LCM735_AsteroidCollision.asteroidCollision(asteroids2)));
+
+        int[] asteroids3 = {10, 2, 5};
+        System.out.println(Arrays.toString(LCM735_AsteroidCollision.asteroidCollision(asteroids3)));
+    }
+}
