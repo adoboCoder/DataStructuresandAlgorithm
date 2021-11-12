abefda1d205f3b43f7982ca5ad6c96fa49d80be2
Kamonchanok Tankul <kamonchanoktankul@kamonchanoksair.lan>
2021-11-11 21:38:14 -0800

    comments

--- LCM_323NumberOfConnectedComponentsInAnUndirectedGraph.java
+++ LCM_323NumberOfConnectedComponentsInAnUndirectedGraph.java
@@ -1,18 +1,23 @@
-public class Solution{
+import java.util.*;
+
+public class LCM_323NumberOfConnectedComponentsInAnUndirectedGraph{
     public int countComponents(int n, int[][] edges){
         if(n <=1 ){
             return n;
         }
+        //Set up adjacency list
         Map<Integer, List<Integer>> map = new HashMap<>();
 
+        //Initialize empty adjacency list
         for(int i = 0; i < n; i++){
             map.put(i, new ArrayList<>());
         }
-
+        //Poppulate adjacency list
         for(int edge[] : edges){
             map.get(edge[0]).add(edge[1]);
             map.get(edge[1]).add(edge[0]);
         }
+        //set up visited set
         Set<Integer> visited = new HashSet<>();
         int count = 0;
         for(int i = 0; i < n; i++){
