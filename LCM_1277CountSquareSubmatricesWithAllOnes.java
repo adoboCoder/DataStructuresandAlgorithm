a89f19937ad12c7d4c813e4112ac98f3ab13329e
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-11-22 09:35:56 -0800

    spacing

--- LCM_1277CountSquareSubmatricesWithAllOnes.java
+++ LCM_1277CountSquareSubmatricesWithAllOnes.java
@@ -1,19 +1,22 @@
-class LCM_1277CountSquareSubmatricesWithAllOnes {
-    public static int countSquares(int[][] matrix) {
-        int mat[][] = new int[matrix.length + 1][matrix[0].length + 1];
+class LCM_1277CountSquareSubMatricesWithAllOnes {
+    public static int countSquares(int[][] dprix) {
+        int dp[][] = new int[dprix.length + 1][dprix[0].length + 1];
         int sum = 0;
-
-        for (int i = 1; i <= matrix.length; i++) {
-            for (int j = 1; j <= matrix[0].length; j++) {
-                if (matrix[i - 1][j - 1] != 0)
-                    sum += (mat[i][j] = Math.min(Math.min(mat[i - 1][j], mat[i][j - 1]), mat[i - 1][j - 1]) + 1);
+        
+        for(int i = 1; i <= dprix.length; i++){
+            for(int j = 1; j <= dprix[0].length; j++){
+                if(dprix[i - 1][j - 1] != 0)
+                    sum += (dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1);
+        
             }
-        }
-        return sum;
+        }      
+    return sum;
     }
 
     public static void main(String[] args) {
-        int[][] matrix = { { 0, 1, 1, 1 }, { 1, 1, 1, 1 }, { 0, 1, 1, 1 } };
-        System.out.println(LCM_1277CountSquareSubmatricesWithAllOnes.countSquares(matrix));
+        int[][] matrix = {  {0,1,1,1},
+                            {1,1,1,1},
+                            {0,1,1,1} };
+        System.out.println(LCM_1277CountSquareSubMatricesWithAllOnes.countSquares(matrix)); 
     }
 }
