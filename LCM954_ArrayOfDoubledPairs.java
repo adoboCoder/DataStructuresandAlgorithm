109768b45911b1d21561e7f645f40c8f4059a531
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-11-24 08:55:32 -0800

    first commit

---
+++ LCM954_ArrayOfDoubledPairs.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class LCM954_ArrayOfDoubledPairs {
+    public static boolean canReorderDoubled(int[] arr) {
+        Arrays.sort(arr);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int n : arr) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        for (int x : arr) {
+            if (map.get(x) == 0)
+                continue;
+            if (x < 0 && x % 2 != 0)
+                return false;
+            int y = x > 0 ? x * 2 : x / 2;
+            if (map.getOrDefault(y, 0) == 0)
+                return false;
+
+            map.put(x, map.get(x) - 1);
+            map.put(y, map.get(y) - 1);
+
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 1, 3, 6 };
+        System.out.println(LCM954_ArrayOfDoubledPairs.canReorderDoubled(arr));
+
+        int[] arr1 = { 4, -2, 2, -4 };
+        System.out.println(LCM954_ArrayOfDoubledPairs.canReorderDoubled(arr1));
+    }
+}
