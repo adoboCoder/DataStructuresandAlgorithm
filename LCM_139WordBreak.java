828b2e90be6647f5707aa4feda01c7ab352950ae
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-11-26 15:05:16 -0800

    Added another test case

--- LCM_139WordBreak.java
+++ LCM_139WordBreak.java
@@ -5,10 +5,10 @@ class LCM_139WordBreak {
         int n = s.length();
         boolean[] dp = new boolean[n + 1];
         dp[0] = true;
-        
-        for(int i = 0; i <= n; i++) {
-            for(int j = i - 1; j >= 0; j--){
-                if(dp[j] && wordDict.contains(s.substring(j, i))){
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (dp[j] && wordDict.contains(s.substring(j, i))) {
                     dp[i] = true;
                     break;
                 }
@@ -20,13 +20,18 @@ class LCM_139WordBreak {
     public static void main(String[] args) {
         List<String> wordDict = new ArrayList<>();
         wordDict.add("cat");
-        wordDict.add("cats");
-        wordDict.add("and");
-        wordDict.add("sand");
         wordDict.add("dog");
-        String s = "catsanddog";
+        wordDict.add("sand");
+        wordDict.add("and");
+        wordDict.add("cats");
+        String s = "catsandog";
         Boolean b = LCM_139WordBreak.wordBreak(s, wordDict);
         System.out.println(b);
-        
+
+        List<String> wordDict1 = new ArrayList<>();
+        wordDict1.add("apple");
+        wordDict1.add("pen");
+        String s1 = "applepenapple";
+        System.out.println(LCM_139WordBreak.wordBreak(s1, wordDict1));
     }
 }
