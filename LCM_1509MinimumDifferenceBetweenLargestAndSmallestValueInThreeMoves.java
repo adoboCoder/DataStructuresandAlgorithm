6b6096acba23b44e09db1557dd07db20dcea6e95
ibaim <thisisaim@gmail.com>
2021-11-27 21:50:36 -0800

    changed test case

--- LCM_1509MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.java
+++ LCM_1509MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.java
@@ -2,33 +2,30 @@ import java.util.*;
 
 class LCM_1509MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves {
     public static int minDifference(int[] nums){
-        if(nums.length < 5) {
+    if(nums.length < 5) {
             return 0;
         }
-        
         PriorityQueue<Integer> minHeap = new PriorityQueue<>(4);
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(4, Collections.reverseOrder());
-
+        
         for(int num : nums) {
             minHeap.add(num);
             maxHeap.add(num);
-
-            if(maxHeap.size() > 4 || minHeap.size() > 4) {
+            if(minHeap.size() > 4 || maxHeap.size() > 4) {
                 minHeap.poll();
                 maxHeap.poll();
             }
         }
-
+        
         int[] smallest = new int[4];
         int[] largest = new int[4];
-
+        
         for(int i = 0; i < 4; i++) {
-            smallest[3 - i] = minHeap.poll();
-            largest[i] = maxHeap.poll();
+            smallest[3 - i] = maxHeap.poll();
+            largest[i] = minHeap.poll();
         }
-
+        
         int result = Integer.MAX_VALUE;
-
         for(int i = 0; i < 4; i++) {
             result = Math.min(result, largest[i] - smallest[i]);
         }
@@ -36,7 +33,7 @@ class LCM_1509MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves {
     }
 
     public static void main(String[] args) {
-        System.out.println(LCM_1509MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.minDifference(new int[]{5, 3, 2, 4}));
+        System.out.println(LCM_1509MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.minDifference(new int[]{1,5,0,10,14}));
         
     }
 }
