958860426f5f091f17e2f256a44824cb14be2d4b
ibaim <thisisaim@gmail.com>
2021-12-04 22:01:28 -0800

    first commit

--- Blind75/Arrays/FindMinimumInRotatedSortedArray.java
+++ Blind75/Arrays/FindMinimumInRotatedSortedArray.java
@@ -16,7 +16,7 @@ public class FindMinimumInRotatedSortedArray {
 
             if(nums[mid] > nums[right]){
                 left = mid + 1;
-            }
+            } 
             else {
                 right = mid;
             }
