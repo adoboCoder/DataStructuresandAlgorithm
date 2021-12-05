6513a54060bf64a488faea64b30f7e048400880f
ibaim <thisisaim@gmail.com>
2021-12-04 16:18:23 -0800

    blind75

---
+++ Blind75/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,18 @@
+package Blind75;
+
+public class BestTimeToBuyAndSellStock {
+    public static int maxProfit(int[] prices) {
+        int currentMax = 0; 
+        int maxSoFar = 0; 
+        for(int i = 1; i < prices.length; i++) {
+            currentMax = Math.max(0, currentMax + (prices[i] - prices[i - 1]));
+            maxSoFar = Math.max(currentMax, maxSoFar);
+        }
+        return maxSoFar;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(BestTimeToBuyAndSellStock.maxProfit(new int[] {7,1,5,3,6,4}));
+        System.out.println(BestTimeToBuyAndSellStock.maxProfit(new int[] {7,6,4,3,1}));
+    }
+}
