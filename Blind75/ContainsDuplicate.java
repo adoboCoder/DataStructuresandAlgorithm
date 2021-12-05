d15b76718442692c6f12aff83800aae82045c2f9
ibaim <thisisaim@gmail.com>
2021-12-04 16:57:33 -0800

    First commit

---
+++ Blind75/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+package Blind75;
+import java.util.*;
+
+public class ContainsDuplicate {
+    public static boolean ContainsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(set.contains(nums[i])) {
+                return false;
+            }
+            set.add(nums[i]);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ContainsDuplicate.ContainsDuplicate(new int[] {1,1,1,3,3,4,3,2,4,2}));
+        System.out.println(ContainsDuplicate.ContainsDuplicate(new int[] {1,2,3,4}));
+    }
+}
---
+++ Blind75/ProductOfArrayExceptSelf.java
@@ -0,0 +1,30 @@
+package Blind75;
+
+import java.util.Arrays;
+
+public class ProductOfArrayExceptSelf  {
+    public static int[] find(int[] nums) {
+        int[] result = new int[nums.length];
+        int left = 1;
+        int right = 1;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(i > 0) {
+                left = left * nums[i - 1];
+            }
+            result[i] = left;
+        }
+
+        for(int i = nums.length - 1; i >= 0; i--) {
+            if(i < nums.length -1) {
+                right = right * nums[i + 1];
+            }
+            result[i] = result[i] * right;
+        }
+        return result; 
+    }
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(ProductOfArrayExceptSelf.find(new int[] {1,2,3,4})));
+        System.out.println(Arrays.toString(ProductOfArrayExceptSelf.find(new int[] {-1,1,0,-3,3})));
+    }
+}
