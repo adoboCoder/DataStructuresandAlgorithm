c58168393c99251cdc881950ea8f57a2bf3152d7
ibaim <thisisaim@gmail.com>
2021-12-04 17:36:18 -0800

    First commit

---
+++ Blind75/Arrays/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,26 @@
+package Blind75.Arrays;
+
+public class FindMinimumInRotatedSortedArray {
+    public static int findMin(int[] nums) {
+        int n = nums.length;
+        
+        if(nums == null || n == 0) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = n -1;
+
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] > nums[right]){
+                left = mid + 1;
+            }
+            else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+}
