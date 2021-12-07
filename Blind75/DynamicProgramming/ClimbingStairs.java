85eb0383a45e84c77c1128fdd45cb4a6c2c6f4a0
ibaim <thisisaim@gmail.com>
2021-12-06 22:09:45 -0800

    first commit

---
+++ Blind75/DynamicProgramming/ClimbingStairs.java
@@ -0,0 +1,25 @@
+package Blind75.DynamicProgramming;
+
+public class ClimbingStairs {
+    public static int ClimbingStairs(int n) {
+
+        if (n <= 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        System.out.println(ClimbingStairs.ClimbingStairs(0));
+        System.out.println(ClimbingStairs.ClimbingStairs(2));
+        System.out.println(ClimbingStairs.ClimbingStairs(3));
+        System.out.println(ClimbingStairs.ClimbingStairs(45));
+    }
+}
