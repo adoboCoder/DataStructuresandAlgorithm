cc81cb57a85562835c466d966f2ce8c7acdbc8d3
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-12-07 16:41:10 -0800

    first commit

--- Blind75/DynamicProgramming/HouseRobber.java
+++ Blind75/DynamicProgramming/HouseRobber.java
@@ -23,6 +23,6 @@ public class HouseRobber {
 
     public static void main(String[] args) {
         System.out.println(HouseRobber.rob(new int[] { 1, 2, 3, 1 }));
-        System.out.println(HouseRobber.rob(new int[] { 2,7,9,3,1 }));
+        System.out.println(HouseRobber.rob(new int[] { 2, 7, 9, 3, 1 }));
     }
 }
--- LCM_91DecodeWays.java
+++ LCM_91DecodeWays.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 class LCM_91DecodeWays {
     public static int numDecodings(String s) {
         if (s == null || s.length() == 0) {
@@ -12,10 +13,10 @@ class LCM_91DecodeWays {
             int first = Integer.valueOf(s.substring(i - 1, i));
             int second = Integer.valueOf(s.substring(i - 2, i));
             if (first >= 1 && first <= 9) {
-               dp[i] += dp[i-1];  
+                dp[i] += dp[i - 1];
             }
             if (second >= 10 && second <= 26) {
-                dp[i] += dp[i-2];
+                dp[i] += dp[i - 2];
             }
         }
         return dp[n];
