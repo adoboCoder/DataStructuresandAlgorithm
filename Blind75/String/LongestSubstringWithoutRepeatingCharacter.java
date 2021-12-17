b2c67412ed6018a8d0c534243fec97c124b5bf31
Kamonchanok Tankul <kamonchanoktankul@kamonchanoksair.lan>
2021-12-16 23:56:09 -0800

    first commit

---
+++ Blind75/String/LongestSubstringWithoutRepeatingCharacter.java
@@ -0,0 +1,28 @@
+package Blind75.String;
+import java.util.*;
+
+public class LongestSubstringWithoutRepeatingCharacter {
+    public static int lengthOfSubstring(String s) {
+        if(s.length() == 0 || s == null) return 0;
+        int result = Integer.MIN_VALUE;
+        int start = 0;
+        int end;
+        Set<Character> set = new HashSet<>();
+
+        for(end = 0; end < s.length(); end++) {
+            char c = s.charAt(end);
+            while(set.contains(s.charAt(end))) {
+                set.remove(s.charAt(start));
+                start++;
+            }
+            set.add(c);
+            result = Math.max(result, end - start + 1);
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        int n = LongestSubstringWithoutRepeatingCharacter.lengthOfSubstring(s);
+        System.out.println(n);
+    }
+}
