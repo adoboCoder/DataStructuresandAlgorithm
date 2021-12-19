86a618d0edb3e7a01d25d9b79261c26b88c51c49
ibaim <thisisaim@gmail.com>
2021-12-18 22:46:04 -0800

    spacing

--- LCM_146LRUCache.java
+++ LCM_146LRUCache.java
@@ -79,25 +79,25 @@ class LRUCache {
         DLinkedNode node = cache.get(key);
 
         if(node == null) {
-          DLinkedNode newNode = new DLinkedNode();
-          newNode.key = key;
-          newNode.value = value;
+            DLinkedNode newNode = new DLinkedNode();
+            newNode.key = key;
+            newNode.value = value;
 
-          cache.put(key, newNode);
-          addNode(newNode);
+            cache.put(key, newNode);
+            addNode(newNode);
 
-          ++size;
+            ++size;
 
-          if(size > capacity) {
+            if(size > capacity) {
             // pop the tail
             DLinkedNode tail = popTail();
             cache.remove(tail.key);
             --size;
-          }
+            }
         } else {
           // update the value.
-          node.value = value;
-          moveToHead(node);
+            node.value = value;
+            moveToHead(node);
         }
     }
 }
