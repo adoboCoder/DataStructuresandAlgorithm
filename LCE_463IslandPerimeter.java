13ea86047a605ea08739edcea93c9131e8efdab2
Kamonchanok Tankul <kamonchanoktankul@kamonchanoksair.lan>
2021-12-19 00:50:17 -0800

    changed algo to dfs

--- LCE_463IslandPerimeter.java
+++ LCE_463IslandPerimeter.java
@@ -1,37 +1,37 @@
 class LCE463IslandPerimeter {
     public static int islandPerimeter(int[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-            
-        int num = 0;
-        
-        for(int i = 0; i < rows; i ++) {
-            for(int j = 0; j < cols; j++) {
+        if(grid == null) return 0;
+
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
                 if(grid[i][j] == 1) {
-                    if(i == 0 || grid[i - 1][j] == 0){ //up
-                        num++;
-                    }
-                    if(j == 0 || grid[i][j - 1] == 0){ //left
-                        num++;
-                    }
-                    if(i == rows - 1 || grid[i + 1][j] == 0) { //down
-                        num++;
-                    }
-                    if(j == cols - 1 || grid[i][j + 1] == 0) { //right
-                        num++;
-                    }
+                    return dfs(grid, i, j);
                 }
             }
         }
-        return num;
+        return 0;
+    }
+    public static int dfs(int[][] grid, int i, int j) {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) return 1;
+        if(grid[i][j] == -1) return 0;
+        if(grid[i][j] == 0) return 1;
+
+        int count = 0;
+        grid[i][j] = -1;
+
+        count+=dfs(grid, i + 1, j);
+        count+=dfs(grid, i, j + 1);
+        count+=dfs(grid, i - 1, j);
+        count+=dfs(grid, i, j - 1);
+        return count;
     }
 
     public static void main(String[] args) {
         int[][] arr =   {   
-                            {0,1,1,1,0},
-                            {1,1,0,1,0},
-                            {1,1,0,0,0},
-                            {0,0,0,0,0}
+                            {0,1,0,0},
+                            {1,1,1,0},
+                            {0,1,0,0},
+                            {1,1,0,0}
                         };
         System.out.println(LCE463IslandPerimeter.islandPerimeter(arr));
     }
