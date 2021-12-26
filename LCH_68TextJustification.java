6061c79b3e955009dfd4fe1e8b219c5c97781444
ibaim <thisisaim@gmail.com>
2021-12-26 06:48:26 -0800

    rewrite

--- LCH_68TextJustification.java
+++ LCH_68TextJustification.java
@@ -3,50 +3,57 @@ import java.util.*;
 class LCH_68TextJustification {
     public static List<String> fullyJustify(String[] words, int maxWidth) {
         List<String> result = new ArrayList<>();
-
-        int index = 0;
-        while (index < words.length) {
-            int count = words[index].length();
-            int last = index + 1;
-            while (last < words.length) {
-                if (words[last].length() + count + 1 > maxWidth)
-                    break;
-                count += words[last].length() + 1;
-                last++;
+        int i = 0;
+        int n = words.length;
+        while(i < n) {
+            int j  = i + 1;
+            int lineLength = words[i].length();
+            while(j < n && (lineLength + words[j].length() + (j - i -1) < maxWidth)) {
+                lineLength += words[j].length();
+                j++;
             }
-
-            StringBuilder sb = new StringBuilder();
-            int diff = last - index - 1;
-            // if last line or number of words in the line is 1, left-justified
-            if (last == words.length || diff == 0) {
-                for (int i = index; i < last; i++) {
-                    sb.append(words[i] + " ");
-                }
-                sb.deleteCharAt(sb.length() - 1);
-                for (int i = sb.length(); i < maxWidth; i++) {
-                    sb.append(" ");
-                }
-            } else {
-                // middle justified
-                int spaces = (maxWidth - count) / diff;
-                int r = (maxWidth - count) % diff;
-                for (int i = index; i < last; i++) {
-                    sb.append(words[i]);
-                    if (i < last - 1) {
-                        for (int j = 0; j <= (spaces + ((i - index) < r ? 1 : 0)); j++) {
-                            sb.append(" ");
-                        }
-                    }
-                }
+            int diff = maxWidth - lineLength;
+            int numberOfWords = j - i;
+            if(numberOfWords == 1 || j >= n) {
+                result.add(leftJustify(words, diff, i, j));
             }
-            result.add(sb.toString());
-            index = last;
+            else {
+                result.add(middleJustify(words, diff, i, j));
+            }
+            i = j;
+            
         }
         return result;
     }
-
+    private static String leftJustify(String[] words, int diff, int i, int j) {
+        int spacesOnRight = diff - (j - i - 1);
+        StringBuilder sb = new StringBuilder(words[i]);
+        for(int k = i +1; k < j; k++) {
+            sb.append(" " + words[k]);
+        }
+        sb.append(" ".repeat(spacesOnRight));
+        return sb.toString();
+    }
+    
+    private static String middleJustify(String[] words, int diff, int i, int j) {
+        int spacesNeeded = j - i - 1;
+        int spaces = diff / spacesNeeded; 
+        int extraSpaces = diff % spacesNeeded;
+        StringBuilder sb = new StringBuilder(words[i]);
+        
+        for(int k = i + 1; k < j; k++) {
+            int spacesToApply = spaces + (extraSpaces-- > 0 ? 1 : 0);
+            sb.append(" ".repeat(spacesToApply) + words[k]);
+        }
+        return sb.toString();
+    }
+//Time: O(lines * maxWidth)
+//Space: O(lines * maxWidth)
     public static void main(String[] args) {
         String[] words = { "This", "is", "an", "example", "of", "text", "justification." };
         System.out.println(LCH_68TextJustification.fullyJustify(words, 16));
+        System.out.println();
+        String[] words1 = {"What","must","be","acknowledgment","shall","be"};
+        System.out.println(LCH_68TextJustification.fullyJustify(words1, 16));
     }
 }
