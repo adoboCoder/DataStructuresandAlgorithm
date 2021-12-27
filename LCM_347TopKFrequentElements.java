932b69eb1b8f810b38c033dbb9648db4a98a8a90
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-12-27 14:58:32 -0800

    Added bucket sort as an alternative

--- LCM_347TopKFrequentElements.java
+++ LCM_347TopKFrequentElements.java
@@ -1,43 +1,69 @@
 import java.util.*;
 
 class LCM_347TopKFrequentElements {
-    public static  int[] topKFrequent(int[] nums, int k) {
+    public static int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        
-        for(int num : nums) {
-            map.put(num , map.getOrDefault(num, 0) +1); 
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<Map.Entry<Integer, Integer>>((a, b) -> a.getValue() - b.getValue());
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<Map.Entry<Integer, Integer>>(
+                (a, b) -> a.getValue() - b.getValue());
 
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             minHeap.add(entry);
-            if(minHeap.size() > k){
+            if (minHeap.size() > k) {
                 minHeap.poll();
             }
         }
 
         List<Integer> topNumbers = new ArrayList<>();
 
-        while(!minHeap.isEmpty()){
+        while (!minHeap.isEmpty()) {
             topNumbers.add(minHeap.poll().getKey());
         }
-            
+
         int[] result = new int[topNumbers.size()];
-        
-        for(int i = 0; i < topNumbers.size(); i++){
+
+        for (int i = 0; i < topNumbers.size(); i++) {
             result[i] = topNumbers.get(i);
         }
-            
+
         return result;
     }
 
-    
-  public static void main(String[] args) {
-    int[] result = LCM_347TopKFrequentElements.topKFrequent(new int[] { 1, 3, 5, 12, 11, 12, 11 }, 2);
-    System.out.println("Here are the K frequent numbers: " + result);
+    public static List<Integer> topKFrequent_bucketSort(int[] nums, int k) {
+        List<Integer>[] bucket = new List[nums.length + 1];
+        Map<Integer, Integer> freqMap = new HashMap<>();
 
-    result = LCM_347TopKFrequentElements.topKFrequent(new int[] { 5, 12, 11, 3, 11 }, 2);
-    System.out.println("Here are the K frequent numbers: " + result);
-  }
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        for (int key : freqMap.keySet()) {
+            int frequency = freqMap.get(key);
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = bucket.length - 1; i >= 0 && result.size() < k; i--) {
+            if (bucket[i] != null) {
+                result.addAll(bucket[i]);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] result = LCM_347TopKFrequentElements.topKFrequent(new int[] { 1, 3, 5, 12, 11, 12, 11 }, 2);
+        System.out.println("Here are the K frequent numbers: " + result);
+
+        result = LCM_347TopKFrequentElements.topKFrequent(new int[] { 5, 12, 11, 3, 11 }, 2);
+        System.out.println("Here are the K frequent numbers: " + result);
+    }
 }
