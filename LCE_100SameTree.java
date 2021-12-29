c92f14b8a671f2f355cf5591d292ace2422fcb01
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-12-29 14:02:47 -0800

    spacing

--- LCE_100SameTree.java
+++ LCE_100SameTree.java
@@ -1,17 +1,16 @@
-class Solution{
-    public boolean isSameTree(TreeNode p, TreeNode q){
-    if(p == null && q == null){
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
             return true;
         }
 
-        if(p == null || q == null){
+        if (p == null || q == null) {
             return false;
         }
 
-        if(p.val == q.val){
-            return isSameTree(p.left, q.left) && isSameTree(p.right,q.right);
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         }
-
         return false;
     }
 }
