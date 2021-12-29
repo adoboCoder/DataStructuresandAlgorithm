5736a9a5344af1467be19343a77572a06c5b80f9
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-12-29 14:08:00 -0800

    First commit

---
+++ Blind75/Tree/SameTree.java
@@ -0,0 +1,33 @@
+package Blind75.Tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class SameTree {
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        if (p.val == q.val) {
+            return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+        }
+        return false;
+    }
+}
