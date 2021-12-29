5ad9834b66fbf27cdd42c07cd04ceb64548aab60
ibaim <thisisaim@gmail.com>
2021-12-28 22:02:55 -0800

    first commit

---
+++ LCM_1091ShortestPathInBinaryMatrix.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class LCM_1091ShortestPathInBinaryMatrix {
+    public static int shortestPathInBinaryMatrix(int[][] grid) {
+        if(grid[0][0] == 1) return -1;
+        int m = grid.length;
+        int n = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] {0, 0, 1}); // x, y, count
+
+        int[][] dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}, {1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
+
+        while(!q.isEmpty()) {
+            int size = q.size();
+            while(size-- > 0) {
+                int[] point = q.poll();
+                if(point[0] == m - 1 && point[1] == n - 1) return point[2];
+
+                for(int[] d : dirs) {
+                    int r = point[0] + d[0];
+                    int c = point[1] + d[1];
+
+                    if(r >= 0 && c >= 0 && r < m && c < n && grid[r][c] == 0) {
+                        q.add(new int[] {r, c, point[2] + 1});
+                        grid[r][c] = 1;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[][] grid =  {
+                            {0, 0, 0},
+                            {1, 1, 0},
+                            {1, 1, 0}
+                        };
+        System.out.println(LCM_1091ShortestPathInBinaryMatrix.shortestPathInBinaryMatrix(grid));
+    }
+}
