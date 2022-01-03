e741474b188b5c635ebce0dd7557453697e090bd
ibaim <thisisaim@gmail.com>
2022-01-02 20:57:56 -0800

    added driver

--- LCM_1877MinimizeMaximumPairSumInArray.java
+++ LCM_1877MinimizeMaximumPairSumInArray.java
@@ -14,4 +14,11 @@ class LCM_1877MinimizeMaximumPairSumInArray {
         }
         return result;
     }
+    public static void main(String[] args) {
+        int[] nums = {3,5,2,3};
+        System.out.println(LCM_1877MinimizeMaximumPairSumInArray.minPariSum(nums));
+        int[] nums2 = {3,5,4,2,4,6};
+        System.out.println(LCM_1877MinimizeMaximumPairSumInArray.minPariSum(nums2));
+        
+    }
 }
