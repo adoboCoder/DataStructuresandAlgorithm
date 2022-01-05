6fd51877a5348b3d98b96509d8e6eb6ca6e3d554
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2022-01-05 15:04:48 -0800

    spacing

--- Blind75/Graph/NumberOfIslands.java
+++ Blind75/Graph/NumberOfIslands.java
@@ -36,10 +36,10 @@ public class NumberOfIslands {
 
     public static void main(String[] args) {
         char[][] grid = {
-                { '1', '1', '1', '1', '0' },
-                { '1', '1', '0', '0', '0' },
-                { '1', '1', '0', '1', '0' },
-                { '0', '0', '0', '1', '1' }
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'0', '0', '0', '1', '1'}
         };
 
         System.out.println(NumberOfIslands.countIslands(grid));
