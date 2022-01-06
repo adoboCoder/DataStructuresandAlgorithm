492aefcba4f72bb72db2285b3c2c42c927770865
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2022-01-06 12:41:40 -0800

    added toArray() and more test cases

--- LCM1762_BuidingsWithAnOceanView.java
+++ LCM1762_BuidingsWithAnOceanView.java
@@ -2,29 +2,34 @@ import java.util.*;
 
 class LCM1762_BuidingsWithAnOceanView {
     public static int[] findBuildings(int[] heights) {
-        List<Integer> list = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
         int last = Integer.MIN_VALUE;
 
         for(int i = heights.length -1; i >= 0; i--){ 
             if(heights[i] > last){
-                list.add(i);
                 last = heights[i];
+                result.add(i);
             }
         }
+        return toArray(result);
+    }
 
-        int[] result = new int[list.size()];
-
-        int index = 0;
-        for(int i = list.size() - 1; i >= 0; i--) {
-            result[index] = list.get(i);
-            index++;
+    private static int[] toArray(List<Integer> result) {
+        int[] arrResult = new int[result.size()];
+        for(int i = 0; i < arrResult.length; i++) {
+            arrResult[i] = result.get(arrResult.length - i - 1);
         }
-        return result;
+        return arrResult;
     }
 
     public static void main(String[] args) {
-        int[] heights = {4, 2, 3, 1};
-        heights = LCM1762_BuidingsWithAnOceanView.findBuildings(heights);
-            System.out.println(Arrays.toString(heights));
+        int[] heights = LCM1762_BuidingsWithAnOceanView.findBuildings(new int[] {4, 2, 3, 1});
+        System.out.println(Arrays.toString(heights));
+
+        int[] heights2 = LCM1762_BuidingsWithAnOceanView.findBuildings(new int[] {4, 3, 2, 1});
+        System.out.println(Arrays.toString(heights2));
+
+        int[] heights3 = LCM1762_BuidingsWithAnOceanView.findBuildings(new int[] {1, 3, 2, 4});
+        System.out.println(Arrays.toString(heights3));
     }
 }
