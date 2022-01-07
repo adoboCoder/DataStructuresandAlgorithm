5d6a8a5ba664f282ac65fec1b954cf46febf2db4
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2022-01-07 13:08:19 -0800

    shortned the code

--- LCM_1138AlphabetBoardPath.java
+++ LCM_1138AlphabetBoardPath.java
@@ -2,36 +2,22 @@
 class LCM_1139AlphabelBoardPath {
 public static String alphabetBoardPath(String target, int rows) {
     // starting point(x,y)
-            int x = 0; 
-            int y = 0;
-            StringBuilder sb = new StringBuilder();
-            for(char c : target.toCharArray()){
-    //target point (i,j)
-                int i = (c - 'a') / rows;  
-                int j = (c - 'a') % rows ;                   
-                    while(x < i){
-                        sb.append('D');
-                        x++;
-                    }
-                    while(x > i){
-                        sb.append('U');
-                        x--;
-                    }
-                    while(y < j){
-                        sb.append('R');
-                        y++;
-                    }
-                    while(y > j){
-                        sb.append('L');
-                        y--;
-                    }
-                    sb.append("!");    
-                }
-                return sb.toString();
+    int x = 0, y = 0;
+    StringBuilder sb = new StringBuilder();
+    for (char c : target.toCharArray()) {
+        int x1 = (c-'a') / rows;
+        int y1 = (c-'a') % rows;
+        while (x1 < x) {x--; sb.append('U');}
+        while (y1 > y) {y++; sb.append('R');}
+        while (y1 < y) {y--; sb.append('L');}
+        while (x1 > x) {x++; sb.append('D');}
+        sb.append('!');
+    }
+    return sb.toString();
 }
     public static void main(String args[]) 
     { 
-        String output = alphabetBoardPath("zzgoogle", 5);
+        String output = alphabetBoardPath("zdz", 5);
         System.out.println(output); 
     } 
 } 
