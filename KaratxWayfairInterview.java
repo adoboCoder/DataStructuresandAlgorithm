1229031f0e26ef0945c98cdb90c68dc5d625d382
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2022-01-21 16:28:41 -0800

    first commit

---
+++ KaratxWayfairInterview.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class KaratxWayfairInterview {
+    public static List<String> findContiguousHistory(String[] array1, String[] array2) {
+
+        String[] longArray = array1;
+        String[] shortArray = array2;
+
+        if (array1.length < array2.length) {
+            shortArray = array1;
+            longArray = array2;
+        } else {
+            shortArray = array2;
+            longArray = array1;
+        }
+
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < longArray.length; i++) {
+            map.put(longArray[i], i);
+        }
+
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < shortArray.length; i++) {
+            List<String> curr = new ArrayList<>();
+            String str = shortArray[i];
+            if (map.containsKey(str)) {
+                int index = map.get(str);
+                int k = i;
+                while (index < longArray.length && k < shortArray.length) {
+                    if (longArray[index].equalsIgnoreCase(shortArray[k])) {
+                        curr.add(shortArray[k]);
+                    } else
+                        break;
+                    index++;
+                    k++;
+                }
+                if (result.size() < curr.size()) {
+                    result = curr;
+                }
+            }
+        }
+
+        return result;
+    }
+}
