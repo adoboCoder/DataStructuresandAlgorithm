5e8a9c083a446412d290fa9ac6feeb6cd525829c
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2022-01-31 12:28:02 -0800

    first commit

---
+++ LCM_791CustomString.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class LCM_791CustomString {
+    public static String customSortString(String order, String s) {
+        Set<Character> set = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
+        for (char c : order.toCharArray()) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == c) {
+                    sb.append(c);
+                }
+            }
+            set.add(c);
+        }
+        for (char c : s.toCharArray()) {
+            if (!set.contains(c)) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String order = "cba";
+        String s = "abcd";
+        System.out.println(LCM_791CustomString.customSortString(order, s));
+        order = "cbafg";
+        s = "abcd";
+        System.out.println(LCM_791CustomString.customSortString(order, s));
+    }
+}
