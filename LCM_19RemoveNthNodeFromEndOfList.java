e7ba0247c3a4d2dfa88c7da55df28c4aa10a0a58
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2022-02-01 10:04:35 -0800

    spacing

--- LCM_19RemoveNthNodeFromEndOfList.java
+++ LCM_19RemoveNthNodeFromEndOfList.java
@@ -1,19 +1,19 @@
-public class Solution {
+class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-       ListNode dummy = new ListNode(0);
-       dummy.next = head;
-       ListNode p = dummy;
-       ListNode runner = dummy;
-       while(n > 0){
-           runner = runner.next;
-           n--;
-       }
-       while(runner.next != null){
-           runner = runner.next;
-           p = p.next;
-       }
-       // p is at the node we want to delete.  
-       p.next = p.next.next;
-       return dummy.next;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode p = dummy;
+        ListNode runner = dummy;
+        while(n > 0){
+            runner = runner.next;
+            n--;
+        }
+        while(runner.next != null){
+            runner = runner.next;
+            p = p.next;
+        }
+        // p is at the node we want to delete.  
+        p.next = p.next.next;
+        return dummy.next;
     }
 }
