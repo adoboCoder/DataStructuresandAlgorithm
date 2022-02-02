3a804d198fe62d7c64998fa4a2da8d7d680793ad
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2022-02-02 15:03:25 -0800

    condition  in for loops were incorrect

--- LCE_766TeoplitzMatrix.java
+++ LCE_766TeoplitzMatrix.java
@@ -1,7 +1,7 @@
 class LCE_766TeoplitzMatrix {
     public static boolean isToeplitzMatrix(int[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
+        for (int i = 0; i < matrix.length - 1; i++) {
+            for (int j = 0; j < matrix[i].length - 1; j++) {
                 if (matrix[i][j] != matrix[i + 1][j + 1])
                     return false;
             }
