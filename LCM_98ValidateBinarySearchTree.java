e5e1e89f1e685c1e00eb15ffc2fc71b35485e805
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-02-01 21:53:28 -0800

    recursive implementation

--- LCM_98ValidateBinarySearchTree.java
+++ LCM_98ValidateBinarySearchTree.java
@@ -1,27 +1,11 @@
-class Solution{
-    public boolean isValidBST(TreeNode root){
-        if(root == null){
-            return true;
-        }
-        return isValid (root, false, 0, false, 0);
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
-
-    private boolean isValid(TreeNode root, boolean hasMax, int max, boolean hasMin, int min) {
-        if(root == null){
-            return true;
-        }
-        else{
-            if(hasMax){
-                if(root.val >= max){
-                    return false;
-                }
-            }
-            if(hasMin){
-                if(root.val <= min){
-                    return false;
-                }
-            }
-        }
-        return isValid(root.left, true, root.val, hasMin, min) &&  isValid(root.right, hasMax, max, true, root.val); 
+    
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) return true;
+        if (root.val >= maxVal || root.val <= minVal) return false;
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
     }
 }
