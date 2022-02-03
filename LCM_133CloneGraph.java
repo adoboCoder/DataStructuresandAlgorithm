6acf1fc3927f72108f95b72a2b518f990b0b5a06
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-02-02 22:02:49 -0800

    refactor

--- LCM_133CloneGraph.java
+++ LCM_133CloneGraph.java
@@ -1,40 +1,16 @@
 import java.util.*;
-
-class Node {
-    public static int val;
-    public static List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-
 class LCM_133CloneGraph {
-    public static HashMap<Integer, Node> map = new HashMap<>();
-    
     public static Node cloneGraph(Node node) {
-        return clone(node);
+        if (node == null) return null;
+        return dfs(node, new HashMap<>());
     }
-
-    public static Node clone(Node node) {
-        if(node == null) return null;
-
-        if(map.containsKey(node.val)) return map.get(node.val);
-
-        Node newNode = new Node(node.val, new ArrayList<Node>());
-        map.put(newNode.val, newNode);
-
-        for(Node neighbor : node.neighbors) {
-            newNode.neighbors.add(clone(neighbor));
-        }
-        return newNode;
+    
+    public static Node dfs(Node node, HashMap<Node, Node> map) {
+        if (map.containsKey(node)) return map.get(node);
+        Node clone = new Node(node.val);
+        map.put(node, clone); // map OLD node to NEW node!
+        for (Node nei : node.neighbors)
+            clone.neighbors.add(dfs(nei, map));
+        return clone;
     }
 }
