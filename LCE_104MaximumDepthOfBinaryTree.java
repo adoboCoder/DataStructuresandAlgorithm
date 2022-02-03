b3ec45601fa253af9adfd3370b40bfb75d07db9c
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2022-02-03 15:37:38 -0800

    removed TreeNode

--- LCE_104MaximumDepthOfBinaryTree.java
+++ LCE_104MaximumDepthOfBinaryTree.java
@@ -1,22 +1,3 @@
-public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 class LCE_104MaximumDepthOfBinaryTree {
     public static int maxDepth(TreeNode root) {
         if (root == null)
