941d5ff6aa8dab49843ebb683c197252d3b96b0d
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2022-02-03 15:40:49 -0800

    removed Treenode

--- LCE_112PathSum.java
+++ LCE_112PathSum.java
@@ -1,22 +1,3 @@
-public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 class LCE_112PathSum {
     public static boolean hasPathSum(TreeNode root, int target) {
         if(root == null) return false;
