3f1b81d01b51c209d3d1e321ec2cc130a87ead11
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-02-05 19:56:07 -0800

    driver

--- LCE_938RangeSumOfBST.java
+++ LCE_938RangeSumOfBST.java
@@ -1,6 +1,7 @@
-class Solution {
+import java.util.*;
+class LCE_938RangeSumOfBST {
     
-    public int rangeSumBST(TreeNode root, int low, int high) {
+    public static int rangeSumBST(TreeNode root, int low, int high) {
         if(root == null){
             return 0;
         }
@@ -15,5 +16,14 @@ class Solution {
             sum = sum + root.val;
         }
         return sum;
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(15);
+        root.right.right = new TreeNode(18);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(7);
+        System.out.println(rangeSumBST(root, 7, 15));
     }
 }
