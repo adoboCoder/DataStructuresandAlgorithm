70777d9fe008dccd81a93a6019d95ed8245625fd
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-02-14 00:01:58 -0800

    Added a solution with constant space

--- LCM_227BasicCalculatorII.java
+++ LCM_227BasicCalculatorII.java
@@ -33,12 +33,45 @@ class LCM_227BasicCalculatorII {
         }
         return result;
     }
+
+    public static int calculateWithOutStack(String s) {
+        if (s == null || s.isEmpty()) return 0;
+
+        int length = s.length();
+        int currentNumber = 0;
+        int lastNumber = 0;
+        int result = 0;
+        char operation = '+';
+        for (int i = 0; i < length; i++) {
+            char currentChar = s.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
+            }
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == length - 1) {
+                if (operation == '+' || operation == '-') {
+                    result += lastNumber;
+                    lastNumber = (operation == '+') ? currentNumber : -currentNumber;
+                } else if (operation == '*') {
+                    lastNumber = lastNumber * currentNumber;
+                } else if (operation == '/') {
+                    lastNumber = lastNumber / currentNumber;
+                }
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        result += lastNumber;
+        return result;
+    }
     public static void main(String[] args) {
-        String s = "3+2*2";
+        String s = "3+23*2";
         System.out.println(LCM_227BasicCalculatorII.calculate(s));
+        System.out.println(LCM_227BasicCalculatorII.calculateWithOutStack(s));
         s = "3/2";
         System.out.println(LCM_227BasicCalculatorII.calculate(s));
+        System.out.println(LCM_227BasicCalculatorII.calculateWithOutStack(s));
         s = "3+8 / 2";
         System.out.println(LCM_227BasicCalculatorII.calculate(s));
+        System.out.println(LCM_227BasicCalculatorII.calculateWithOutStack(s));
     }
 }
