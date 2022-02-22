528fbd3e4bf9567c68631b2136da2a12ced17b49
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-02-21 21:32:45 -0800

    static methods

--- LCM_958CheckCompleteNessOfABinaryTree.java
+++ LCM_958CheckCompleteNessOfABinaryTree.java
@@ -1,13 +1,13 @@
-class Solution {
-    public boolean isCompleteTree(TreeNode root) {
+class LCM_958CheckCompletenessOfABinaryTree {
+    public static boolean isCompleteTree(TreeNode root) {
         return isCompleteTree(root,0,countNodes(root));
     }
-    private boolean isCompleteTree(TreeNode root, int index, int total) { 
+    private static boolean isCompleteTree(TreeNode root, int index, int total) { 
         if (root == null)  return true; 
         else if (index >= total) return false; 
         return isCompleteTree(root.left, 2 * index + 1, total) && isCompleteTree(root.right, 2 * index + 2, total); 
     } 
-     private  int countNodes(TreeNode root) { 
+    private static int countNodes(TreeNode root) { 
         if (root == null) return 0; 
         return 1 + countNodes(root.left) + countNodes(root.right); 
     } 
