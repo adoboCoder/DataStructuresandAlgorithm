9e6d32ee9e387c3a784c3a4b8ff1345d1813f69d
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-02-21 23:15:19 -0800

    driver, tc, sc

--- LCH_273IntegerToEnglishWords.java
+++ LCH_273IntegerToEnglishWords.java
@@ -1,21 +1,21 @@
-class Solution {
-    private final String[] belowTen = new String[] {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-    private final String[] belowTwenty = new String[] {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    private final String[] belowHundred = new String[] {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+class LCH_273IntegerToEnglishWords {
+    private static final String[] belowTen = new String[] {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    private static final String[] belowTwenty = new String[] {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private static final String[] belowHundred = new String[] {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
     
-    public String numberToWords(int num) {
+    public static String numberToWords(int num) {
         if (num == 0) return "Zero";
         return helper(num); 
     }
     
-    private String helper(int num) {
+    private static String helper(int num) {
         String result = new String();
 
         if (num < 10) {
             result = belowTen[num];
         }
         else if (num < 20) {
-            result = belowTwenty[num -10];
+            result = belowTwenty[num - 10];
         }
         else if (num < 100) {
             result = belowHundred[num/10] + " " + helper(num % 10);
@@ -33,5 +33,12 @@ class Solution {
             result = helper(num/1000000000) + " Billion " + helper(num % 1000000000);
         }
         return result.trim();
+    }
+    public static void main(String[] args) {
+        System.out.println(numberToWords(100));
+        System.out.println(numberToWords(12345));
+        System.out.println(numberToWords(1234567));
+        System.out.println(numberToWords(0));
+        
     }
 }
