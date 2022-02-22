a70927547defc3908edc7dd23c82f9c30958a464
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-02-21 21:31:59 -0800

    driver, tc, sc

--- LCE_896MonotonicArray.java
+++ LCE_896MonotonicArray.java
@@ -1,16 +1,31 @@
-class Solution {
-    public boolean isMonotonic(int[] nums) {
-        int inc=1;
-        int dec=1;
-        for(int i=1; i<nums.length; i++){
-            if(nums[i] - nums[i-1] > 0)
+class LCE_896MonotonicArray {
+    public static boolean isMonotonic(int[] nums) {
+        int inc = 1;
+        int dec = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] - nums[i - 1] > 0)
                 inc++;
-            else if(nums[i] - nums[i-1] < 0)
+            else if(nums[i] - nums[i - 1] < 0)
                 dec++;
             else {
-                inc++; dec++;
+                inc++;
+                dec++;
             }
         }
         return inc == nums.length || dec == nums.length;
     }
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,3};
+        int[] nums2 = {6,5,4,4};
+        int[] nums3 = {1,3,2};
+
+        System.out.println(isMonotonic(nums));
+        System.out.println(isMonotonic(nums2));
+        System.out.println(isMonotonic(nums3));
+
+    }
 }
+/*
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
