9ebc464a1d2a99b2f895c70db0283f3b808ef33a
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-03-05 22:17:04 -0800

    added node class

--- LCM_426ConvertBinarySearchTreeToSortedDoublyLinkedList.java
+++ LCM_426ConvertBinarySearchTreeToSortedDoublyLinkedList.java
@@ -1,5 +1,21 @@
 import java.util.*;
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
 
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,Node _left,Node _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
 class LCM_426ConvertBinarySearchTreeToSortedDoublyLinkedList{
     public static Node treeToDoublyList(Node root) {
         Node prev = null;
