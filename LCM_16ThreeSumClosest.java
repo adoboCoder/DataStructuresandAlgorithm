66b5beb55593abdcb85f35fae33ba156971162ae
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-03-18 07:51:56 -0700

    first commit

---
+++ LCM_16ThreeSumClosest.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class LCM_16ThreeSumClosest {
+    public static int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int sum = nums[0] + nums[1] + nums[nums.length - 1];
+        int closestSum = sum;
+        
+        for(int i = 0; i < nums.length - 2; i++){
+            if(i == 0 || nums[i] != nums[i-1]){
+                int left = i + 1;
+                int right = nums.length - 1;
+                while(left < right){
+                    sum = nums[left] + nums[right] + nums[i];
+                    if(sum < target){
+                        //move closer to target sum.
+                        while(left < right && nums[left] == nums[left+1]){
+                            left++;
+                        }
+                        left++;
+                    }else if(sum > target){
+                        //move closer to target sum.
+                        while(left < right && nums[right] == nums[right-1]){
+                            right--;
+                        }
+                        right--;
+                    }else{
+                        return sum;
+                    }
+                    //update the closest sum if needed.
+                    if(Math.abs(target - sum) < Math.abs(target - closestSum)){
+                        closestSum = sum;
+                    }
+                }
+            }
+        }
+        return closestSum;
+    }
+    public static void main(String[] args) {
+        int[] nums = {-1, 2, 1, -4};
+        int target = 1;
+
+        System.out.println(threeSumClosest(nums, target));
+    }
+}
