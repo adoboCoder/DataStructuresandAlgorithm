9ecf333827ecae384c9a8b339e2549630cbad770
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-03-24 12:43:57 -0700

    test case

--- LCE_543DiameterOfBinaryTree.java
+++ LCE_543DiameterOfBinaryTree.java
@@ -24,15 +24,8 @@ class LCE_543DiameterOfBinaryTree {
     root.left = new TreeNode(2);
     root.right = new TreeNode(3);
     root.left.left = new TreeNode(4);
-    root.right.left = new TreeNode(5);
-    root.right.right = new TreeNode(6);
-    System.out.println("Tree Diameter: " + LCE_543DiameterOfBinaryTree.diameterOfBinaryTree(root));
-    root.left.left = null;
-    root.right.left.left = new TreeNode(7);
-    root.right.left.right = new TreeNode(8);
-    root.right.right.left = new TreeNode(9);
-    root.right.left.right.left = new TreeNode(10);
-    root.right.right.left.left = new TreeNode(11);
+    root.left.right = new TreeNode(5);
+    
     System.out.println("Tree Diameter: " + LCE_543DiameterOfBinaryTree.diameterOfBinaryTree(root));
   }
 }
