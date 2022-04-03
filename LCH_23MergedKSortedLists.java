90c2040cf02306afe97ee9429c2be2af74ea6272
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-04-03 12:40:05 -0700

    tc sc

--- LCH_23MergedKSortedLists.java
+++ LCH_23MergedKSortedLists.java
@@ -63,3 +63,17 @@ class LCH_23MergedKSortedLists{
 
     }
 }
+
+/*
+
+Time complexity : O(N log k)  where k is the number of linked lists.
+
+The comparison cost will be reduced to O(log k) for every pop and insertion to priority queue. But finding the node with the smallest value just costs O(1) time.
+There are NN nodes in the final linked list.
+
+Space complexity :
+
+O(n) Creating a new linked list costs O(n) space.
+O(k) The code above present applies in-place method which cost O(1) space. And the priority queue (often implemented with heaps) costs O(k) space (it's far less than N in most situations).
+
+*/
