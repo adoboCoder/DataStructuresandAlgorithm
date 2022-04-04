387ddbbf24fef4ea34f35f02760fb664fc9a9669
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-04-03 19:46:03 -0700

    changed variable names

--- LCM_791CustomSortString.java
+++ LCM_791CustomSortString.java
@@ -1,21 +1,21 @@
 import java.util.*;
 
 class LCM_791CustomSortString {
-    public static String customSortString(String S, String T) {
+    public static String customSortString(String order, String s) {
         int[] count = new int[26];
-        for (char c : T.toCharArray()) {
+        for (char c : s.toCharArray()) {
             ++count[c - 'a']; 
         }  // count each char in T.
         StringBuilder sb = new StringBuilder();
-        for (char c : S.toCharArray()) {                            
+        for (char c : order.toCharArray()) {                            
             while (count[c - 'a']-- > 0) {
                 sb.append(c); 
-            }    // sort chars both in T and S by the order of S.
+            }    // sort chars both in s and ORDER by the order of ORDER.
         }
         for (char c = 'a'; c <= 'z'; ++c) {
             while (count[c - 'a']-- > 0) {
                 sb.append(c); 
-            }    // group chars in T but not in S.
+            }    // group chars in s but not in ORDER.
         }
         return sb.toString();
     }
