73fde6fa35c482dd7030bfb85309e51888891207
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-04-04 23:43:36 -0700

    node + tc sc

--- LCM_863AllNodesDistanceKInBinaryTree.java
+++ LCM_863AllNodesDistanceKInBinaryTree.java
@@ -1,51 +1,63 @@
 import java.util.*;
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 class LCM_863AllNodesDistanceKInBinaryTree {
             
     public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
-        List<Integer> res = new LinkedList<>();
+        List<Integer> result = new LinkedList<>();
         if (K == 0) {
-            res.add(target.val);
+            result.add(target.val);
         } else {
-            dfs(res, root, target.val, K ,0);
+            dfs(result, root, target.val, K ,0);
         }
-        return res;
+        return result;
     }
     
-    private int dfs(List<Integer> res, TreeNode node, int target, int K, int depth) {
+    private int dfs(List<Integer> result, TreeNode node, int target, int K, int depth) {
         if (node == null) return 0;
         
         if (depth == K) {
-            res.add(node.val);
+            result.add(node.val);
             return 0;
         }
         
-        int left, right;
+        int left;
+        int right;
         if (node.val == target || depth > 0) {
-            left = dfs(res, node.left, target, K, depth + 1);
-            right = dfs(res, node.right, target, K, depth + 1);
+            left = dfs(result, node.left, target, K, depth + 1);
+            right = dfs(result, node.right, target, K, depth + 1);
         } else {
-            left = dfs(res, node.left, target, K, depth);
-            right = dfs(res, node.right, target, K, depth);
+            left = dfs(result, node.left, target, K, depth);
+            right = dfs(result, node.right, target, K, depth);
         }
         
         if (node.val == target) return 1;
         
         if (left == K || right == K) {
-            res.add(node.val);
+            result.add(node.val);
             return 0;
         }
         
         if (left > 0) {
-            dfs(res, node.right, target, K, left + 1);
+            dfs(result, node.right, target, K, left + 1);
             return left + 1;
         }
         
         if (right > 0) {
-            dfs(res, node.left, target, K, right + 1);
+            dfs(result, node.left, target, K, right + 1);
             return right + 1;
         }
         
         return 0;
     }
 }
+
+// TC O(N)
+// SC O(
