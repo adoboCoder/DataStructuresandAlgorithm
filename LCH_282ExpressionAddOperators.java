0391b3e0ca15f3a428e07f7dc2540f40ff5de1da
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-04-04 21:55:17 -0700

    changed multed to prevNumber

--- LCH_282ExpressionAddOperators.java
+++ LCH_282ExpressionAddOperators.java
@@ -2,29 +2,29 @@ import java.util.*;
 
 class LCH_282ExpressionAddOperators {
     public static List<String> addOperators(String num, int target) {
-        List<String> rst = new ArrayList<String>();
-        if(num == null || num.length() == 0) return rst;
-        helper(rst, "", num, target, 0, 0, 0);
-        return rst;
+        List<String> result = new ArrayList<String>();
+        if(num == null || num.length() == 0) return result;
+        helper(result, "", num, target, 0, 0, 0);
+        return result;
     }
-    public static void helper(List<String> rst, String path, String num, int target, int pos, long eval, long multed){
+    public static void helper(List<String> result, String path, String num, int target, int pos, long eval, long prevNumber){
         if(pos == num.length()){
             if(target == eval)
-                rst.add(path);
+                result.add(path);
             return;
         }
         for(int i = pos; i < num.length(); i++){
             if(i != pos && num.charAt(pos) == '0') break;
             long cur = Long.parseLong(num.substring(pos, i + 1));
             if(pos == 0){
-                helper(rst, path + cur, num, target, i + 1, cur, cur);
+                helper(result, path + cur, num, target, i + 1, cur, cur);
             }
             else{
-                helper(rst, path + "+" + cur, num, target, i + 1, eval + cur , cur);
+                helper(result, path + "+" + cur, num, target, i + 1, eval + cur , cur);
                 
-                helper(rst, path + "-" + cur, num, target, i + 1, eval -cur, -cur);
+                helper(result, path + "-" + cur, num, target, i + 1, eval -cur, -cur);
                 
-                helper(rst, path + "*" + cur, num, target, i + 1, eval - multed + multed * cur, multed * cur );
+                helper(result, path + "*" + cur, num, target, i + 1, eval - prevNumber + prevNumber * cur, prevNumber * cur );
             }
         }
     }
