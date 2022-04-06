4498e8616fa55f05733f3439a7d293f7bafec86e
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-04-05 21:29:13 -0700

    driver tc sc

--- LCE_270ClosestBinarySearchTreeValue.java
+++ LCE_270ClosestBinarySearchTreeValue.java
@@ -1,5 +1,5 @@
-class Solution {
-    public int closestValue(TreeNode root, double target) {
+class LCE_270ClosestBinarySearchTreeValue {
+    public static int closestValue(TreeNode root, double target) {
       int a = root.val;
       TreeNode child = target < a ? root.left : root.right;
       if (child == null){
@@ -8,4 +8,14 @@ class Solution {
       int b = closestValue(child, target);
       return Math.abs(a - target) < Math.abs(b - target) ? a : b;
     }
+  public static void main(String[] args) {
+      TreeNode root = new TreeNode(4);
+      root.left = new TreeNode(2);
+      root.right = new TreeNode(5);
+      root.left.left = new TreeNode(1);
+      root.left.right = new TreeNode(3);
+      System.out.println(closestValue(root, 3.714286));
+  }
 }
+// tc O(H) 
+// sc O(1)
