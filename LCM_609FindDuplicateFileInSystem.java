f2c29d829040459a48596ecfa536946c638ad120
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-06-21 11:47:20 -0700

    first commit

---
+++ LCM_609FindDuplicateFileInSystem.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class LCM_609FindDuplicateFileInSystem {
+    public static List<List<String>> findDuplicate(String[] paths) {
+        List<List<String>> result = new ArrayList<List<String>>();
+        int n = paths.length;
+        if(n == 0) return result;
+
+        Map<String, Set<String>> map = new HashMap<>();
+        for (String path : paths) {
+            String[] strs = path.split("\\s+");
+            for (int i = 1; i < strs.length; i++) {
+                int idx = strs[i].indexOf("(");
+                String content = strs[i].substring(idx);
+                String filename = strs[0] + "/" + strs[i].substring(0, idx);
+                Set<String> filenames = map.getOrDefault(content, new HashSet<String>());
+                filenames.add(filename);
+                map.put(content, filenames);
+            }
+        }
+        
+        for (String key : map.keySet()) {
+            if (map.get(key).size() > 1) {
+                result.add(new ArrayList<String>(map.get(key)));
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        String[] content = {
+                "root/a 1.txt(abcd) 2.txt(efgh)","root/c 3.txt(abcd)","root/c/d 4.txt(efgh)","root 4.txt(efgh)"};
+        System.out.println(LCM_609FindDuplicateFileInSystem.findDuplicate(content));
+    }
+}
