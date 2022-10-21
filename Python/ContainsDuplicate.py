a8ae10f400f09bbd768ab18c2d813b9d135fa1ef
Aim Verapornpongkul <aim.verapornpongkul@intentsify.io>
2022-10-21 15:58:53 -0700

    Contains duplicate, python

--- Python/ContainsDuplicate.py
+++ Python/ContainsDuplicate.py
@@ -1,5 +1,5 @@
 class Solution:
-    def containsDuplicate(nums) -> bool:
+    def contains_duplicate(nums) -> bool:
         if len(nums) == len(set(nums)):
             return False
         else:
@@ -7,4 +7,5 @@ class Solution:
     
     if __name__ == '__main__':
         nums = [1,2,3,1]
-        print(containsDuplicate(nums))
+        print(contains_duplicate(nums))
+
