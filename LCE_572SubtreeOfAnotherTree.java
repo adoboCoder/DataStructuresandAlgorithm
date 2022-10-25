b17e8b09e1f4278be836048b2311ffeb9c7e9647
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-10-25 09:41:54 -0700

    initial commit

---
+++ LCE_572SubtreeOfAnotherTree.java
@@ -0,0 +1,24 @@
+class LCE_572SubtreeOfAnotherTree{
+    public static boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (isEqualTree(root, subRoot)) return true;
+        if (root == null) return false;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private static boolean isEqualTree(TreeNode root1, TreeNode root2) {
+        if (root1 == null || root2 == null) return root2 == root1;
+        return root1.val == root2.val && isEqualTree(root1.left, root2.left) && isEqualTree(root1.right, root2.right);
+    }
+}
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
