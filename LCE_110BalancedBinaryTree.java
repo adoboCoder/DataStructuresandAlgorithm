66f5222ae49bf4a61ac3e14f162a6ef0984a4b92
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-10-24 22:06:10 -0700

    indent

--- LCE_110BalancedBinaryTree.java
+++ LCE_110BalancedBinaryTree.java
@@ -1,18 +1,18 @@
 class LCE_110BalancedBinaryTree {
-private boolean result = true;
+    private boolean result = true;
 
-public boolean isBalanced(TreeNode root) {
-    maxDepth(root);
-    return result;
-}
+    public boolean isBalanced(TreeNode root) {
+        maxDepth(root);
+        return result;
+    }
 
-public int maxDepth(TreeNode root) {
-    if (root == null)
-        return 0;
-    int l = maxDepth(root.left);
-    int r = maxDepth(root.right);
-    if (Math.abs(l - r) > 1)
-        result = false;
-    return 1 + Math.max(l, r);
-}
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+        if (Math.abs(l - r) > 1)
+            result = false;
+        return 1 + Math.max(l, r);
+    }
 }
