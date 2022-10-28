df3be407266a1b9cc7e26095620141b73a6c109f
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-10-28 09:11:47 -0700

    more understandable code

--- LCH_10RegularExpressionMatching.java
+++ LCH_10RegularExpressionMatching.java
@@ -1,41 +1,41 @@
 class LCH_10RegularExpressionMatching {
     public static boolean isMatch(String s, String p) {
-        if(s == null || p == null) {
-            return false;
-        }
+        boolean[][] cache = new boolean[s.length() + 1][p.length() + 1];
 
-        boolean[][] dp = new boolean [s.length() + 1][p.length() + 1];
-        dp[0][0] = true;
-
-        for(int i = 0; i < p.length(); i ++) {
-            if(p.charAt(i) == '*' && dp[0][i - 1]) {
-                dp[0][i + 1] = true;
-            }
-        }
-
-        for (int i = 0 ; i < s.length(); i++) {
-            for (int j = 0; j < p.length(); j++) {
-                if (p.charAt(j) == '.') {
-                    dp[i+1][j+1] = dp[i][j];
-                }
-                if (p.charAt(j) == s.charAt(i)) {      
-                    dp[i+1][j+1] = dp[i][j];
-                }
-                if (p.charAt(j) == '*') {
-                    if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {
-                        dp[i+1][j+1] = dp[i+1][j-1];
-                    } else {
-                        dp[i+1][j+1] = (dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]);
-                    }
-                }
-            }
-        }
-        return dp[s.length()][p.length()];
+        return dfs(cache, s, p, 0, 0);
     }
+
+    private static boolean dfs(boolean[][] cache, String s, String p, int i, int j) {
+        if (cache[i][j] != false) return cache[i][j];
+
+        if (i >= s.length() && j >= p.length()) return true;
+
+        if (j >= p.length()) return false;
+
+        boolean match =
+            i < s.length() &&
+            (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
+
+        if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+            cache[i][j] =
+                dfs(cache, s, p, i, j + 2) ||
+                (match && dfs(cache, s, p, i + 1, j));
+        } else {
+            cache[i][j] = match && dfs(cache, s, p, i + 1, j + 1);
+        }
+
+        return cache[i][j];
+    } 
 
     public static void main(String[] args) {
         String s = "mississippi";
         String p = "mis*is*p*.";
+        System.out.println(LCH_10RegularExpressionMatching.isMatch(s,p));
+        s = "aa";
+        p = "a*";
+        System.out.println(LCH_10RegularExpressionMatching.isMatch(s,p));
+        s = "aab";
+        p = "c*a*b";
         System.out.println(LCH_10RegularExpressionMatching.isMatch(s,p));
     }
 }
