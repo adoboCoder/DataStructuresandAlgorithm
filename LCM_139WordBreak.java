203bf8c713529718ced07b96788a6c91814b8bc3
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-11-01 22:37:33 -0700

    long line

--- LCM_139WordBreak.java
+++ LCM_139WordBreak.java
@@ -8,10 +8,7 @@ class LCM_139WordBreak {
 
         for (int i = s.length() - 1; i >= 0; i--) {
             for (String word : wordDict) {
-                if (
-                    ((i + word.length()) <= s.length()) &&
-                    (s.substring(i, i + word.length()).startsWith(word))
-                ) {
+                if (((i + word.length()) <= s.length()) && (s.substring(i, i + word.length()).startsWith(word))) {
                     dp[i] = dp[i + word.length()];
                 }
                 if (dp[i]) {
