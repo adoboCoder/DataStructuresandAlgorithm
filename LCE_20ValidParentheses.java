2fd388da33f6f60734629fadf54a9781bfdb37c0
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-11-09 10:35:53 -0800

    .

--- LCE_20ValidParentheses.java
+++ LCE_20ValidParentheses.java
@@ -1,30 +1,30 @@
 import java.util.Stack;
-class LCE_20ValidParentheses {
-    public static boolean isValid(String s){
-        Stack<Character> stack  = new Stack<>();
 
-        for(char c : s.toCharArray()){
-            if(c == '('){
+class LCE_20ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
                 stack.push(')');
-            }else if( c == '['){
+            } else if (c == '[') {
                 stack.push(']');
-            }
-            else if( c == '{'){
+            } else if (c == '{') {
                 stack.push('}');
-            }
-            else if (stack.isEmpty() || stack.pop() != c){
+            } else if (stack.isEmpty() || stack.pop() != c) {
                 return false;
             }
         }
         return stack.isEmpty();
     }
+
     public static void main(String[] args) {
         String s = "()";
-        System.out.println(isValid(s));
+        System.out.println(LCE_20ValidParentheses.isValid(s));
         String s2 = "()[]{}";
-        System.out.println(isValid(s2));
+        System.out.println(LCE_20ValidParentheses.isValid(s2));
         String s3 = "(]";
-        System.out.println(isValid(s3));
+        System.out.println(LCE_20ValidParentheses.isValid(s3));
     }
 }
 
@@ -32,13 +32,17 @@ class LCE_20ValidParentheses {
 // Space: O(n)
 
 /*
-1. Build a stack
-2. keep pushing closed corresponding characters ), }, or ] as long as the characters are open (, {, or [
-3. If encounter a closed character, then check if stack is empty(nothing was pushed on the stack) or
-    the top of the stack is != current character(closed paren).
-4. If either conidion in 3 is true, there is no need to check further since it won't be matching. Note that we're popping the stack each time.
-5. If all conditions above were passed, it measn we're at the end of the string.  If the stack is empty then it is a valid paren (return true)
-    else they do not match(return false)
-
-
-*/
+ * 1. Build a stack
+ * 2. keep pushing closed corresponding characters ), }, or ] as long as the
+ * characters are open (, {, or [
+ * 3. If encounter a closed character, then check if stack is empty(nothing was
+ * pushed on the stack) or
+ * the top of the stack is != current character(closed paren).
+ * 4. If either conidion in 3 is true, there is no need to check further since
+ * it won't be matching. Note that we're popping the stack each time.
+ * 5. If all conditions above were passed, it measn we're at the end of the
+ * string. If the stack is empty then it is a valid paren (return true)
+ * else they do not match(return false)
+ * 
+ * 
+ */
--- LCM_17LetterCombinationsOfAPhoneNumber.java
+++ LCM_17LetterCombinationsOfAPhoneNumber.java
@@ -1,42 +1,42 @@
 import java.util.*;
 
-class Leetcode {
+class LCM_17LetterCombinationsOfAPhoneNumber {
     public static List<String> letterCombinations(String digits) {
         ArrayList<String> result = new ArrayList<>();
-        
-        //Check input and call DFS
-        if(digits != null && digits.length() > 0){
-            String[] map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        // Check input and call DFS
+        if (digits != null && digits.length() > 0) {
+            String[] map = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
             dfs(digits, map, result, new StringBuilder(), 0);
         }
         return result;
     }
-    
-    private static void dfs(String digits, String[] map, ArrayList<String> result, StringBuilder sb, int index){
-        //base case for dfs to exit recursion
-        if(index == digits.length()){
+
+    private static void dfs(String digits, String[] map, ArrayList<String> result, StringBuilder sb, int index) {
+        // base case for dfs to exit recursion
+        if (index == digits.length()) {
             result.add(sb.toString());
             return;
         }
-        
+
         int digit = Character.getNumericValue(digits.charAt(index));
         String letters = map[digit];
-        
-        //Adding each letter to the string we've built so far
-        for(int i = 0; i < letters.length(); i++){
+
+        // Adding each letter to the string we've built so far
+        for (int i = 0; i < letters.length(); i++) {
             char c = letters.charAt(i);
             sb.append(c);
             dfs(digits, map, result, sb, index + 1);
             sb.deleteCharAt(sb.length() - 1);
         }
-        
+
     }
-    
+
     public static void main(String[] args) {
-        System.out.println(Leetcode.letterCombinations("23"));
+        System.out.println(LCM_17LetterCombinationsOfAPhoneNumber.letterCombinations("23"));
     }
 }
 /*
-Time: O(n 4^n) because w maps to wxyz, and 7 maps to pqrs "9999"
-Space: O(n)
-*/
+ * Time: O(n 4^n) because w maps to wxyz, and 7 maps to pqrs "9999"
+ * Space: O(n)
+ */
