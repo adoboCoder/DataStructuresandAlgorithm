5b59fab5c7e50569dc1ea53cf9ea7f95e1288d35
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2022-11-30 21:28:28 -0800

    driver code

--- LCM_1239MaximumLengthOfAConcatenatedStringwithUniqueCharacters.java
+++ LCM_1239MaximumLengthOfAConcatenatedStringwithUniqueCharacters.java
@@ -1,40 +1,52 @@
-class Solution{
-    private int result = 0;
+import java.util.*;
 
-    public int maxLength(List<String> arr){
-        if(arr == null || arr.size() == 0){
+class LCM_1239MaximumLengthofaConcatenatedStringwithUniqueCharacters {
+    private static int result = 0;
+
+    public static int maxLength(List<String> arr) {
+        if (arr == null || arr.size() == 0) {
             return 0;
         }
         dfs(arr, "", 0);
         return result;
     }
 
-    public void dfs(List<String> arr, String path, int index){
+    public static void dfs(List<String> arr, String path, int index) {
         boolean isUniqueChar = isUniqueCharacter(path);
 
-        if(isUniqueChar){
+        if (isUniqueChar) {
             result = Math.max(path.length(), result);
         }
 
-        if(index == arr.size() || !isUniqueChar){
+        if (index == arr.size() || !isUniqueChar) {
             return;
         }
 
-        for(int i = index; i < arr.size(); i++){
+        for (int i = index; i < arr.size(); i++) {
             dfs(arr, path + arr.get(i), i + 1);
         }
     }
 
-    public boolean isUniqueCharacter(String str) {
+    public static boolean isUniqueCharacter(String str) {
         Set<Character> hs = new HashSet<>();
 
-        for(char c : str.toCharArray()){
-            if(!hs.add(c)){
+        for (char c : str.toCharArray()) {
+            if (!hs.add(c)) {
                 return false;
             }
             hs.add(c);
         }
         return true;
-        
+
+    }
+
+    public static void main(String[] args) {
+        List<String> arr = new ArrayList<>();
+        arr.add("un");
+        arr.add("iq");
+        arr.add("ue");
+
+        System.out.println(LCM_1239MaximumLengthofaConcatenatedStringwithUniqueCharacters.maxLength(arr));
+
     }
 }
